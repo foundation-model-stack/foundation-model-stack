064f0b72a074429a44594f3e9b63291fbce71176
Brian Vaughan <bvaughan@ibm.com>
2023-10-06 22:20:58 +0000

    early draft of a tuning script

---
+++ fms/training/plugins.py
@@ -0,0 +1,176 @@
+from abc import abstractmethod
+from datetime import datetime
+import os
+from pathlib import Path
+from typing import Dict, List
+import torch
+from torch import distributed as dist
+from torch import nn
+from fms import utils
+from fms.utils import print0, generation
+
+
+class TrainerPlugin:
+    """
+    A TrainerPlugin runs once every epoch, and possibly every `steps` steps.
+    It is passed relevant objects that can be used for checkpointing, logging,
+    or validation.
+    """
+    def __init__(self, steps=None):
+        self.steps = steps
+
+    def run(self, step):
+        """
+        Whether or not to run this plugin on the current step.
+        """
+        # if step is None, we're at an epoch end not an intermediate step.
+        # By default we always run for epoch ends.
+        if step is None:
+            return True
+        # If self.steps is None, we're only recording epoch ends and this isn't one.
+        if self.steps is None:
+            return False
+        # record every `step` steps
+        if (step + 1) % self.steps == 0:
+            return True
+        return False
+
+    @abstractmethod
+    def step(self, model: nn.Module, optimizer, epoch:int, metrics: Dict = {}, step:int=None):
+        pass
+
+
+class InferenceValidator(TrainerPlugin):
+    """
+    A training plugin to print the results of running inference on a given prompt.
+    """
+    def __init__(self, prompt_tokens: List[str], tokenizer, device, steps=None, eos_token=None):
+        super().__init__(steps)
+        self.tokenizer = tokenizer
+        input_ids = tokenizer.convert_tokens_to_ids(prompt_tokens)
+        self.input_ids = torch.tensor(input_ids, dtype=torch.long, device=device)
+        self.eos_token_id = None if eos_token is None else self.tokenizer.convert_tokens_to_ids([eos_token])[0]
+
+    def step(self, model: nn.Module, optimizer, epoch: int, metrics: Dict={}, step: int=None):
+        if not self.run(step):
+            return
+        training = model.training
+        model.eval()
+        with torch.no_grad():
+            curtime = datetime.now().strftime("%H:%M.%S")
+            prefix = f"{curtime}:{epoch:02d}"
+            if step is not None:
+                prefix = prefix + f":{step:04d}"
+
+            result = generation.generate(model, self.input_ids)
+            result = generation.truncate_after_eos(result, self.eos_token_id)
+            result = self.tokenizer.convert_ids_to_tokens(result)
+            result = self.tokenizer.convert_tokens_to_string(result)
+            print0("generated result:", result)
+        model.train(training)
+
+class MetricReporter(TrainerPlugin):
+    """
+    A training plugin to periodically log metrics
+    """
+    # TODO: add optional validation dataloader and validation loss.
+    # TODO: add `writer` functions that handles logging metrics to experiment
+    # tracking tools such as aimstack/wandb/neptune (or add alternate plugin?)
+    def __init__(self, seconds=3, writer=print0):
+        self.seconds = seconds
+        self.last_reported_time = datetime.now()
+        self.tokens_seen = 0
+        self.last_reported_step = -1
+        self.cum_loss = 0
+        self.last_step = -1
+        self.writer = writer
+    def step(self, model: nn.Module, optimizer, epoch: int, metrics: Dict={}, step: int=None):
+        if "batch_size" in metrics and "input_length" in metrics:
+            self.tokens_seen += metrics["batch_size"] * metrics["input_length"]
+        if "loss" in metrics:
+            self.cum_loss += metrics["loss"]
+
+        current_time = datetime.now()
+        elapsed = (current_time - self.last_reported_time).total_seconds()
+
+        if step is None:
+            step = self.last_step + 1
+            self.last_step = 0
+            steps = step - self.last_reported_step
+            self.last_reported_step = 0
+        else:
+            self.last_step = step
+            if elapsed < self.seconds or step == self.last_reported_step:
+                return
+            steps = step - self.last_reported_step
+            self.last_reported_step = step
+
+        to_report = {}
+        if "loss" in metrics:
+            to_report["loss"] = f"{metrics['loss']:.4f}"
+        to_report["avg_loss"] = f"{self.cum_loss / steps:.4f}"
+
+        more_metrics = {
+            "tok/stp": f"{self.tokens_seen / steps:,.1f}",
+            "tok/s": f"{self.tokens_seen / elapsed:,.1f}",
+        }
+        if torch.cuda.is_available() and utils.has_package("pynvml"):
+            nvidia_metrics = {
+                "gpu_mem_use" : f"{torch.cuda.memory_usage()}%",
+                "gpu_utzn" : f"{torch.cuda.utilization()}%"
+            }
+            more_metrics.update(nvidia_metrics)
+
+        self.last_reported_time = current_time
+        self.tokens_seen = 0
+        self.cum_loss = 0
+        to_report.update(more_metrics)
+        self.writer(epoch, step, current_time, to_report)
+
+
+class Checkpointer(TrainerPlugin):
+    """
+    A training plugin to write checkpoints.
+    TODO: This will require changes to handle distributed checkpoints.
+    """
+    def __init__(
+        self,
+        save_dir: Path = Path("./checkpoints"),
+        steps: int = None,
+        name: str = None,
+        group: dist.ProcessGroup = None,
+    ):
+        super().__init__(steps)
+        os.makedirs(save_dir, exist_ok=True)
+        if type(save_dir) == str:
+            save_dir = os.path.expanduser(save_dir)
+            save_dir = Path(save_dir)
+        self.save_dir = save_dir
+        self.group = group
+        self.name = name
+
+    # TODO: this probably also needs to accept a dataset since we want to
+    # support checkpointable datasets.
+    def step(self, model: nn.Module, optimizer, epoch: int, metrics: Dict={}, step: int=None):
+        if not self.run(step):
+            return
+
+        model_name = (
+            model.__class__.__name__.lower() if self.name is None else self.name
+        )
+        model_dict = model.state_dict()
+        optim_dict = optimizer.state_dict()
+        state_dict = {"model": model_dict, "optimizer": optim_dict, "epoch": epoch}
+        if step is not None:
+            file = f"{model_name}_{epoch:03d}_{step+1:05d}"
+        else:
+            file = f"{model_name}_{epoch:03d}_final"
+        save_dir = self.save_dir
+        if self.group is None:
+            file = save_dir / f"{file}.pth"
+        else:
+            file = save_dir / file
+            os.makedirs(file, exist_ok=True)
+            file = file / f"rank_{self.group.rank():02d}.pth"
+        print0("Writing checkpoint", file)
+        torch.save(state_dict, file)
---
+++ scripts/train_causal.py
@@ -0,0 +1,205 @@
+import argparse
+import os
+from pathlib import Path
+from typing import List
+import torch
+from torch import distributed as dist
+from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
+from torch import nn
+from torch.utils.data import DataLoader
+from fms import models
+from fms.datasets import instructions, text
+from fms.distributed.strategy import TensorParallelStrategy
+from fms.models import llama
+from fms.models.hf.llama.modeling_llama_hf import LLaMAHFForCausalLM
+from fms.models.llama import LLaMA, LLaMAConfig
+from fms.utils import print0, tokenizers
+from fms.training import trainer, plugins as trainplugins
+
+parser = argparse.ArgumentParser(
+    description="Script to train or tune a model"
+)
+parser.add_argument("--epochs", type=int, default=1)
+parser.add_argument("--report_steps", type=int, default=500, help="Run the reporting function every report_steps steps")
+parser.add_argument("--checkpoint_steps", type=int, default=None, help="If > 0, Checkpoint every checkpoint_steps steps within a single epoch.")
+parser.add_argument("--device_type", type=str, default=None, help="Device type. If not specified check for availability of cuda, mps, then cpu")
+parser.add_argument(
+    "--distributed",
+    type=str,
+    default=None,
+    help="For a distributed job (multiple instances with RANK+WORLD_SIZE) specify fsdp or tp. Default None",
+)
+parser.add_argument("--output_path", type=str, default='./checkpoints', help="Output directory to save trained model checkpoints")
+parser.add_argument("--model_path", type=str, default=None, help="Resume from a checkpoint a the given path")
+parser.add_argument(
+    "--tokenizer",
+    type=str,
+    default="char_tokenizer",
+    help="Name of or path to the tokenizer (e.g. ~/tokenizer.model)",
+)
+
+parser.add_argument("--peft_method", type=str, default=None, help="Peft method (lora, ...). Default None if not using peft")
+
+args = parser.parse_args()
+
+local_rank = int(os.getenv("LOCAL_RANK", 0))
+world_size = int(os.getenv("WORLD_SIZE", 1))
+
+# default search for what's available
+device_type = args.device_type
+if device_type is None:
+    if torch.cuda.is_available():
+        device_type = 'cuda'
+    elif torch.backends.mps.is_available():
+        device_type = 'mps'
+    else:
+        device_type = 'cpu'
+
+device = torch.device(device_type, local_rank)
+
+group = None
+
+if args.distributed is not None:
+    dist.init_process_group(backend='nccl', rank=local_rank, world_size=world_size)
+    group = dist.GroupMember.WORLD
+
+
+def get_loss_fn():
+    ce_loss = torch.nn.CrossEntropyLoss()
+    def loss_fn(output, target):
+        # model output is batch x seq_len x vocab_size.
+        # ce expects batch x vocab x seq_len
+        output = output.transpose(-1, -2)
+        return ce_loss(output, target)
+    return loss_fn
+
+def get_micro_model() -> nn.Module:
+    _micro_llama_config = LLaMAConfig(
+        emb_dim=192, nheads=4, nlayers=5, max_expected_seq_len=1024, src_vocab_size=256
+    )
+    model = LLaMA(_micro_llama_config)
+    model = model.to(device)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4)
+    epoch = 0
+    if args.model_path is not None:
+        # if we already started training, tune from where we left off.
+        state_dict = torch.load(args.model_path)
+        model.load_state_dict(state_dict['model'])
+        optimizer.load_state_dict(state_dict['optimizer'])
+        # the saved epoch is the last completed one, we'll start at the next epoch
+        epoch = state_dict['epoch'] + 1
+    return model, optimizer, epoch
+
+def get_llama():
+    model = llama.load_fms_llama(args.model_path)
+    model.to(device)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4)
+    epoch = 0
+    print0("model loaded")
+    return model, optimizer, epoch
+
+
+def hf_to_fms_model(model):
+    model.orig_forward_f = model.forward
+    def new_forward(self, *args, **kwargs):
+        hf_out = model.orig_forward_f(self, *args, **kwargs)
+        logits = hf_out.logits
+        kv_cache = hf_out.past_key_values
+        if "use_cache" in kwargs and kwargs["use_cache"]:
+            return logits, kv_cache
+        else:
+            return logits
+    model.forward = new_forward
+    return model
+
+
+def peft_model(model):
+    """
+    Converts an fms model to an PEFT and HF adapted (wrapped) model, while
+    preserving the original `forward` function.
+
+    If we call state_dict() on one of these models we'll get keys prefixed with
+    `base_model.model.*`, so correctly saving and re-loading one of these
+    requires some care. The state_dict will also contain all paramters, not 
+    just the adapter, though we plan to use merged tuned models for now.
+    """
+    model = LLaMAHFForCausalLM.from_fms_model(model)
+    from peft import get_peft_config, get_peft_model, LoraConfig
+    from peft.mapping import PeftModelForCausalLM
+    lora_config = LoraConfig(
+        r=8,
+        lora_alpha=32,
+        target_modules=["query", "key"],
+        bias= "none",
+        task_type= "CAUSAL_LM",
+        lora_dropout=0.05,
+        inference_mode=False
+    )
+
+    if args.peft_method == 'lora':
+        model = PeftModelForCausalLM(model, lora_config, adapter_name="None")
+    else:
+        # TODO: add others
+        raise ValueError("unsupported peft method", args.peft_method)
+
+    model = hf_to_fms_model(model)
+    return model
+
+
+def main():
+
+    print0("Loading model...")
+    torch.set_default_device(device)
+
+    # micro llama model for faster testing
+    #model, optimizer, epoch = get_micro_model()
+    model, optimizer, epoch = get_llama()
+    print("model loaded", local_rank)
+
+    model = model.to(device)
+    tokenizer = tokenizers.get_tokenizer(args.tokenizer)
+
+    if args.peft_method is not None:
+        model = peft_model(model)
+
+    bos_token = '<s>' # for char tokenizer should be 2 and 3. TODO add to tokenizer?
+    eos_token = '</s>'
+    bos_token_id = tokenizer.convert_tokens_to_ids([bos_token])[0]
+    eos_token_id = tokenizer.convert_tokens_to_ids([eos_token])[0]
+
+    # for use with micro model
+    # dataset = text.shakespeare(pad_token=' ').to(device)
+
+    dataset = instructions.JsonInstructions(
+        "/home/bvaughan/alpaca_data.json",
+        tokenizer=tokenizer, max_len=2048, device=device,
+        bos_tok_id=bos_token_id, eos_tok_id=eos_token_id
+    )
+
+    dataloader = DataLoader(dataset)
+
+    #model.to(torch.bfloat16)
+
+    loss_fn = get_loss_fn()
+
+    # todo, should batch these.
+    sample_prompt = {"instruction" : "What is the meaning of life?"}
+    sample_prompt = dataset.make_prompt(sample_prompt)
+    sample_prompt2 = {"instruction" : "Please provide a recipe for chicken soup."}
+    sample_prompt2 = dataset.make_prompt(sample_prompt2)
+
+    sample_prompt = [bos_token] + tokenizer.tokenize(sample_prompt)
+    sample_prompt2 = [bos_token] + tokenizer.tokenize(sample_prompt2)
+
+    # sample_prompt = args.validation_prompt
+    validator = trainplugins.InferenceValidator(sample_prompt, tokenizer, device, steps=args.report_steps, eos_token=eos_token)
+    validator2 = trainplugins.InferenceValidator(sample_prompt2, tokenizer, device, steps=args.report_steps, eos_token=eos_token)
+    checkpointing = trainplugins.Checkpointer(steps=args.checkpoint_steps, group=group, save_dir=args.output_path)
+    reporting = trainplugins.MetricReporter()
+
+    plugins = [reporting, validator, validator2, checkpointing]
+    print0("training...")
+    trainer.train(model, optimizer, dataloader, loss_fn, start_epoch=epoch, epochs=args.epochs, trainer_plugins=plugins, grad_accum_iters=10)
+
+if __name__ == '__main__':
+    main()
