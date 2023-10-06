06948ac69673aca2f9d2971aca8ba17d24ce15f3
Joshua Rosenkranz <jmrosenk@us.ibm.com>
2023-10-06 01:37:49 -0400

    chore: AbsolutePositionalEmbedding refactored to AbsolutePositionEmbedding

--- tests/modules/test_embedding.py
+++ tests/modules/test_embedding.py
@@ -2,15 +2,15 @@ import math
 
 import torch
 
-from fms.modules.embedding import WordEmbedding, AbsolutePositionalEmbedding
+from fms.modules.embedding import WordEmbedding, AbsolutePositionEmbedding
 
 
-def test_absolute_positional_embedding_correct_pads():
+def test_absolute_position_embedding_correct_pads():
     """Test that absolute position embeddings correctly corrects for pads in middle of sequence"""
     L = 50
     for pad_id in range(L):
         for insert in range(L):
-            m = AbsolutePositionalEmbedding(L, 1, padding_idx=pad_id, max_pos=L)
+            m = AbsolutePositionEmbedding(L, 1, padding_idx=pad_id, max_pos=L)
             x = list(range(L))
             x = x[:pad_id] + x[pad_id + 1 :]
             x_pad = x[:insert] + [pad_id] + x[insert:]
@@ -28,13 +28,13 @@ def test_absolute_positional_embedding_correct_pads():
                 ), f"Index {i} of nonpadded output {y[i]} does not match padded output {y_[i]} with pad token {pad_id}"
 
 
-def test_absolute_positional_embedding_provides_position_ids():
+def test_absolute_position_embedding_provides_position_ids():
     """test that absolution positional embeddings will use the given position ids properly when not correcting for pads and position_ids provided"""
     vocab_size = 50
     emb_dim = 1
     padding_idx = 0
     max_pos = 5
-    emb = AbsolutePositionalEmbedding(
+    emb = AbsolutePositionEmbedding(
         vocab_size, emb_dim, padding_idx=padding_idx, max_pos=max_pos
     )
     x = torch.IntTensor([0, 0, 0, 1, 2]).unsqueeze(0)
@@ -56,13 +56,13 @@ def test_absolute_positional_embedding_provides_position_ids():
     assert torch.equal(expected, actual)
 
 
-def test_absolute_positional_embedding_provides_position_ids_correct_pads():
+def test_absolute_position_embedding_provides_position_ids_correct_pads():
     """test that absolution positional embeddings will use the given position ids properly when correcting for pads and position_ids provided"""
     vocab_size = 50
     emb_dim = 1
     padding_idx = 0
     max_pos = 10
-    emb = AbsolutePositionalEmbedding(
+    emb = AbsolutePositionEmbedding(
         vocab_size, emb_dim, padding_idx=padding_idx, max_pos=max_pos
     )
     x = torch.IntTensor([0, 0, 0, 1, 2]).unsqueeze(0)
