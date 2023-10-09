fe0df4f4c6b2e15616baf4d0e43b90fa7388d602
Antoni Viros <aviros@ibm.com>
2023-10-09 18:32:37 +0000

    Update tests and init

--- fms/models/llama.py
+++ fms/models/llama.py
@@ -194,6 +194,9 @@ class LLaMA(nn.Module):
             ntk_scaling=self.config.ntk_scaling,
             max_seq_len=self.config.max_expected_seq_len,
         )
+        self._update_cached_freqs_cis(
+            self.config.max_expected_seq_len, self.shared.emb.weight.device
+        )
 
         self.layers = []
         for i in range(self.config.nlayers):
@@ -244,13 +247,16 @@ class LLaMA(nn.Module):
         seq_len = x.shape[1]
         # the max start position should be based on the max first position of each sequence
         max_start_pos = torch.max(position_ids[:, 0]) if position_ids is not None else 0
+        self._update_cached_freqs_cis(max_start_pos + seq_len, x.device)
+
+    def _update_cached_freqs_cis(self, max_seq_len, device=None):
         if isinstance(self.distributed_strategy, UniformModelParallelStrategy):
             for dev_idx in set(self.distributed_strategy.layer_to_device.values()):
                 self.rot_emb.compute_freqs_cis(
-                    torch.device("cuda", dev_idx), max_start_pos + seq_len
+                    torch.device("cuda", dev_idx), max_seq_len
                 )
         else:
-            self.rot_emb.compute_freqs_cis(x.device, max_start_pos + seq_len)
+            self.rot_emb.compute_freqs_cis(device, max_seq_len)
 
     def _helper(
         self,
