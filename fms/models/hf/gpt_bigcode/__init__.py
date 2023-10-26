271fe80fe73a741f66f6aed0f16f678b684711e3
Joshua Rosenkranz <jmrosenk@us.ibm.com>
2023-10-26 11:52:50 -0400

    fix: addressed pr comments; added convert_to_hf method; now accepting a model in get_model which will not delete layers, but if name or path are given, the layers will be deleted

--- fms/models/hf/gpt_bigcode/__init__.py
+++ fms/models/hf/gpt_bigcode/__init__.py
@@ -1,22 +1,27 @@
 import os
 from typing import Union
 
+import torch
+from transformers import PreTrainedModel, GPTBigCodeForCausalLM, GPTBigCodeConfig
+
 from fms.models.hf.gpt_bigcode.modeling_gpt_bigcode_hf import (
     HFAdaptedGPTBigCodeForCausalLM,
 )
 
 
 def get_model(
-    model_name_or_path: Union[str, os.PathLike]
+    model_name_or_path: Union[str, os.PathLike, PreTrainedModel],
 ) -> HFAdaptedGPTBigCodeForCausalLM:
     """
     Get a Huggingface adapted FMS model from an equivalent HF model
 
     Parameters
     ----------
-    model_name_or_path: Union[str, os.PathLike]
-        Either the name of the model in huggingface hub or the absolute path to
-        the huggingface model
+    model_name_or_path: Union[str, os.PathLike, PreTrainedModel]
+        Either the name of the model in huggingface hub, the absolute path to
+        the huggingface model, or the huggingface model itself. If the
+        model_name_or_path is a PreTrainedModel, it will not be deleted from
+        memory
 
     Returns
     -------
@@ -29,7 +34,11 @@ def get_model(
     from fms.models.gpt_bigcode import GPTBigCode
 
     register_fms_models()
-    hf_model = AutoModelForCausalLM.from_pretrained(model_name_or_path)
+    hf_model_in_memory = isinstance(model_name_or_path, PreTrainedModel)
+    if hf_model_in_memory:
+        hf_model = model_name_or_path
+    else:
+        hf_model = AutoModelForCausalLM.from_pretrained(model_name_or_path)
 
     model = GPTBigCode(
         src_vocab_size=hf_model.config.vocab_size,
@@ -56,6 +65,10 @@ def get_model(
             model.base_model.layers[i].attn.key.bias.copy_(k_bias)
             model.base_model.layers[i].attn.value.weight.copy_(v)
             model.base_model.layers[i].attn.value.bias.copy_(v_bias)
+            # if the model was not provided to us, we can assume we don't want
+            # the layers to persist in memory
+            if not hf_model_in_memory:
+                del layer
         model.head.weight.copy_(hf_model.lm_head.weight)
     hf_model_fms = HFAdaptedGPTBigCodeForCausalLM.from_fms_model(
         model=model,
@@ -89,3 +102,92 @@ def __rename_weights_to_fms(orig_sd):
         new_sd[new_name] = param
 
     return new_sd
+
+
+def convert_to_hf(
+    fms_hf_model: HFAdaptedGPTBigCodeForCausalLM,
+) -> GPTBigCodeForCausalLM:
+    """
+    Convert an HF-Adapted FMS GPTBigCode model to and HF model
+
+    Parameters
+    ----------
+    fms_hf_model: HFAdaptedGPTBigCodeForCausalLM
+        the HF-Adapted FMS GPTBigCode model
+
+    Returns
+    -------
+    GPTBigCodeForCausalLM
+        an HF equivalent model
+    """
+    hf_config = fms_hf_model.config
+    oss_hf_model = GPTBigCodeForCausalLM(
+        GPTBigCodeConfig(
+            vocab_size=hf_config.vocab_size,
+            n_embd=hf_config.hidden_size,
+            layer_norm_epsilon=hf_config.ln_eps,
+            n_head=hf_config.nheads,
+            n_layer=hf_config.nlayers,
+            n_inner=int(hf_config.hidden_size * hf_config.hidden_grow_factor),
+            pad_token_id=hf_config.pad_token_id,
+            bos_token_id=hf_config.bos_token_id,
+            eos_token_id=hf_config.eos_token_id,
+            n_positions=hf_config.max_pos,
+            scale_attention_softmax_in_fp32=False,
+        )
+    )
+
+    with torch.no_grad():
+
+        oss_hf_model.transformer.wte.weight.copy_(
+            fms_hf_model.decoder.model.embedding.weight
+        )
+        oss_hf_model.transformer.wpe.weight.copy_(
+            fms_hf_model.decoder.model.position_embedding.weight
+        )
+        for i, oss_hf_layer in enumerate(oss_hf_model.transformer.h):
+            fms_hf_layer = fms_hf_model.decoder.model.layers[i]
+
+            # self attn
+            oss_hf_layer.attn.c_attn.weight.copy_(
+                torch.cat(
+                    [
+                        fms_hf_layer.attn.query.weight.data,
+                        fms_hf_layer.attn.key.weight.data,
+                        fms_hf_layer.attn.value.weight.data,
+                    ],
+                    dim=0,
+                )
+            )
+            oss_hf_layer.attn.c_attn.bias.copy_(
+                torch.cat(
+                    [
+                        fms_hf_layer.attn.query.bias.data,
+                        fms_hf_layer.attn.key.bias.data,
+                        fms_hf_layer.attn.value.bias.data,
+                    ],
+                    dim=0,
+                )
+            )
+            oss_hf_layer.attn.c_proj.weight.copy_(fms_hf_layer.attn.dense.weight)
+            oss_hf_layer.attn.c_proj.bias.copy_(fms_hf_layer.attn.dense.bias)
+
+            # mlp
+            oss_hf_layer.mlp.c_fc.weight.copy_(fms_hf_layer.ff_sub_layer.w1.weight)
+            oss_hf_layer.mlp.c_fc.bias.copy_(fms_hf_layer.ff_sub_layer.w1.bias)
+            oss_hf_layer.mlp.c_proj.weight.copy_(fms_hf_layer.ff_sub_layer.w2.weight)
+            oss_hf_layer.mlp.c_proj.bias.copy_(fms_hf_layer.ff_sub_layer.w2.bias)
+
+            # layer norm
+            oss_hf_layer.ln_1.weight.copy_(fms_hf_layer.ln.weight)
+            oss_hf_layer.ln_1.bias.copy_(fms_hf_layer.ln.bias)
+            oss_hf_layer.ln_2.weight.copy_(fms_hf_layer.ff_ln.weight)
+            oss_hf_layer.ln_2.bias.copy_(fms_hf_layer.ff_ln.bias)
+        oss_hf_model.transformer.ln_f.weight.copy_(
+            fms_hf_model.decoder.model.dec_norm.weight
+        )
+        oss_hf_model.transformer.ln_f.bias.copy_(
+            fms_hf_model.decoder.model.dec_norm.bias
+        )
+
+    return oss_hf_model
--- tests/models/hf/test_gpt_bigcode_hf.py
+++ tests/models/hf/test_gpt_bigcode_hf.py
@@ -9,6 +9,7 @@ from transformers import (
 import torch
 
 from fms.models.gpt_bigcode import GPTBigCode
+from fms.models.hf.gpt_bigcode import convert_to_hf
 from fms.models.hf.gpt_bigcode.configuration_gpt_bigcode_hf import (
     HFAdaptedGPTBigCodeConfig,
 )
@@ -57,79 +58,7 @@ class HFAdaptedGPTBigCodeFixtures(
     def oss_hf_model(
         self, fms_hf_model: HFAdaptedGPTBigCodeForCausalLM
     ) -> PreTrainedModel:
-        hf_config = fms_hf_model.config
-        oss_hf_model = GPTBigCodeForCausalLM(
-            GPTBigCodeConfig(
-                vocab_size=hf_config.vocab_size,
-                n_embd=hf_config.hidden_size,
-                layer_norm_epsilon=hf_config.ln_eps,
-                n_head=hf_config.nheads,
-                n_layer=hf_config.nlayers,
-                n_inner=int(hf_config.hidden_size * hf_config.hidden_grow_factor),
-                pad_token_id=hf_config.pad_token_id,
-                bos_token_id=hf_config.bos_token_id,
-                eos_token_id=hf_config.eos_token_id,
-                n_positions=hf_config.max_pos,
-                scale_attention_softmax_in_fp32=False,
-            )
-        )
-
-        with torch.no_grad():
-
-            oss_hf_model.transformer.wte.weight.copy_(
-                fms_hf_model.decoder.model.embedding.weight
-            )
-            oss_hf_model.transformer.wpe.weight.copy_(
-                fms_hf_model.decoder.model.position_embedding.weight
-            )
-            for i, oss_hf_layer in enumerate(oss_hf_model.transformer.h):
-                fms_hf_layer = fms_hf_model.decoder.model.layers[i]
-
-                # self attn
-                oss_hf_layer.attn.c_attn.weight.copy_(
-                    torch.cat(
-                        [
-                            fms_hf_layer.attn.query.weight.data,
-                            fms_hf_layer.attn.key.weight.data,
-                            fms_hf_layer.attn.value.weight.data,
-                        ],
-                        dim=0,
-                    )
-                )
-                oss_hf_layer.attn.c_attn.bias.copy_(
-                    torch.cat(
-                        [
-                            fms_hf_layer.attn.query.bias.data,
-                            fms_hf_layer.attn.key.bias.data,
-                            fms_hf_layer.attn.value.bias.data,
-                        ],
-                        dim=0,
-                    )
-                )
-                oss_hf_layer.attn.c_proj.weight.copy_(fms_hf_layer.attn.dense.weight)
-                oss_hf_layer.attn.c_proj.bias.copy_(fms_hf_layer.attn.dense.bias)
-
-                # mlp
-                oss_hf_layer.mlp.c_fc.weight.copy_(fms_hf_layer.ff_sub_layer.w1.weight)
-                oss_hf_layer.mlp.c_fc.bias.copy_(fms_hf_layer.ff_sub_layer.w1.bias)
-                oss_hf_layer.mlp.c_proj.weight.copy_(
-                    fms_hf_layer.ff_sub_layer.w2.weight
-                )
-                oss_hf_layer.mlp.c_proj.bias.copy_(fms_hf_layer.ff_sub_layer.w2.bias)
-
-                # layer norm
-                oss_hf_layer.ln_1.weight.copy_(fms_hf_layer.ln.weight)
-                oss_hf_layer.ln_1.bias.copy_(fms_hf_layer.ln.bias)
-                oss_hf_layer.ln_2.weight.copy_(fms_hf_layer.ff_ln.weight)
-                oss_hf_layer.ln_2.bias.copy_(fms_hf_layer.ff_ln.bias)
-            oss_hf_model.transformer.ln_f.weight.copy_(
-                fms_hf_model.decoder.model.dec_norm.weight
-            )
-            oss_hf_model.transformer.ln_f.bias.copy_(
-                fms_hf_model.decoder.model.dec_norm.bias
-            )
-
-        return oss_hf_model
+        return convert_to_hf(fms_hf_model)
 
 
 class TestHFAdaptedGPTBigCode(
