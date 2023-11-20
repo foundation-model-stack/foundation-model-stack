d3e0bec62429f29d5cb2cfd9c664f09913c822d2
Davis Wertheimer <davis.wertheimer@ibm.com>
2023-11-20 15:04:37 -0500

    Diagnostic prints pt2

--- eval_discrete.py
+++ eval_discrete.py
@@ -133,6 +133,8 @@ def speculative_generate(
     n_kv_s = past_key_value_states
     while n_gen < max_new_tokens:
         print("n_gen:", n_gen)
+        print(len(result))
+        
         n_steps += 1
         input_ids = next_input[:, -max_seq_len:]
         
@@ -160,6 +162,9 @@ def speculative_generate(
 #         input_ids = input_ids[0].unsqueeze(0).expand(25,-1)
         
         torch.cuda.empty_cache()
+        cudastats = torch.cuda.memory_summary(device=torch.cuda.current_device(), abbreviated=True)
+        print(cudastats)
+        
         output = model.forward(input_ids, include_embeds=True, mask=mask, **kwargs)
         
         logits, past_key_value_states, embeds = output
