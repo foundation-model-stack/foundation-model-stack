3c568018d147b6ed50d6ac584cc4de7c5e1cff23
Davis Wertheimer <davis.wertheimer@ibm.com>
2023-12-04 14:53:59 -0500

    Fix emb dim

--- eval_recur_13.py
+++ eval_recur_13.py
@@ -240,7 +240,7 @@ def speculative_generate(
         result = result[0]
     return result, n_steps
 
-test = Speculator(n_heads=3)
+test = Speculator(n_heads=3, emb_dim=5120)
 test.load_state_dict(torch.load("/lustre/dwertheimer/results/llama-speculator/gen4/recur-n2_PAhsdp_ws48_mbs8_sl4096_pr0_vFMS9352761_jid3016_sysAwsEfa0/checkpoints/step_15000_ckp.pth", map_location="cpu")["model_state"])
 test.cuda()
 
