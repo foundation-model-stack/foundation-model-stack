d05866191a8c4a25462c7c25033b9a5b4b98ec47
Antoni Viros <aviros@ibm.com>
2024-01-29 22:39:31 +0000

    Fix bug on adapters with fusable weights
    ghstack-source-id: 705d95045998a0c85d8e6ae79b27e7a4cbac9670
    Pull Request resolved: https://github.com/foundation-model-stack/foundation-model-stack/pull/164

--- fms/utils/serialization.py
+++ fms/utils/serialization.py
@@ -293,6 +293,8 @@ def load_state_dict_into_model(
                 fms_partial_sd = adapter(partial_sd)
             except FusableWeightsMissingError as e:
                 for weight in e.missing_weights:
+                    used_keys.add(weight)
+                    
                     partial_sd[weight] = state_dict[weight]
                     if partial_sd[weight].device != initial_device:
                         partial_sd[weight] = partial_sd[weight].to(
