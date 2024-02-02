b842088e26a9f9d0a81198f74b9ad4865322a63a
Davis Wertheimer <davis.wertheimer@ibm.com>
2024-02-02 17:17:18 -0500

    Fix closedfms imports pt4

--- fms/datasets/__init__.py
+++ fms/datasets/__init__.py
@@ -4,22 +4,22 @@ from fms.datasets.instructions import JsonInstructions
 from fms.datasets import text
 from fms.utils.tokenizers import BaseTokenizer
 
-__dataset_factory: Mapping[str, Callable[[str, BaseTokenizer], Dataset] | type] = {
-    "instruction": JsonInstructions,
-    "text": text.causaltext,
-}
+# __dataset_factory: Mapping[str, Callable[[str, BaseTokenizer], Dataset] | type] = {
+#     "instruction": JsonInstructions,
+#     "text": text.causaltext,
+# }
 
 
-def get_dataset(name: str, tokenizer: BaseTokenizer, data: str = "", **kwargs):
-    """
-    Get a dataset by type.
+# def get_dataset(name: str, tokenizer: BaseTokenizer, data: str = "", **kwargs):
+#     """
+#     Get a dataset by type.
 
-    Args:
-    name: Which style of dataset to use. E.g. instruction, text.
-    data: The data this dataset should load. E.g. a url or file system path.
-    """
-    name = name.lower()
-    if name not in __dataset_factory:
-        raise NameError(f"Dataset name should be one of {__dataset_factory.keys()}")
-    ctr = __dataset_factory[name]
-    return ctr(data, tokenizer, **kwargs)
+#     Args:
+#     name: Which style of dataset to use. E.g. instruction, text.
+#     data: The data this dataset should load. E.g. a url or file system path.
+#     """
+#     name = name.lower()
+#     if name not in __dataset_factory:
+#         raise NameError(f"Dataset name should be one of {__dataset_factory.keys()}")
+#     ctr = __dataset_factory[name]
+#     return ctr(data, tokenizer, **kwargs)
