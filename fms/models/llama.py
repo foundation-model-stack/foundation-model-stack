ad94ddc0dd60af371796131a29fef37b2e017e6c
Davis Wertheimer <davis.wertheimer@ibm.com>
2024-02-14 11:36:46 -0500

    Re-blacking (no pre-commit file for this repo?)

--- fms/models/llama.py
+++ fms/models/llama.py
@@ -228,10 +228,10 @@ class LLaMA(nn.Module):
     def reset_parameters(self):
         # Call reset_parameters for relevant sub-layers
         for m in self.modules():
-            if (isinstance(m, MultiHeadAttention) or 
-                isinstance(m, WordEmbedding) or
-                isinstance(m, GatedLinearUnit) or
-                isinstance(m, LayerNormParameterized)
+            if (isinstance(m, MultiHeadAttention) 
+                or isinstance(m, WordEmbedding)
+                or isinstance(m, GatedLinearUnit)
+                or isinstance(m, LayerNormParameterized)
             ):
                 m.reset_parameters()
 
