602907e50f2de0a17c9fb65c1ca55d19f5143410
Antoni Viros <aviros@ibm.com>
2024-03-06 18:26:44 +0000

    Update (base update)
    [ghstack-poisoned]

--- fms/modules/attention.py
+++ fms/modules/attention.py
@@ -292,12 +292,12 @@ class TPMultiHeadAttention(MultiHeadAttention, TPModule):
             position_encoder,
             gain,
         )
-        self.pretp_kvheads = kvheads
+        self.pre_tp_kvheads = kvheads
         self.setup_tp(rank, world_size)
 
     def colwise_param_names(self) -> List[str]:
         colwise_weights = ["query"]
-        if self.pretp_kvheads != 1:
+        if self.pre_tp_kvheads != 1:
             colwise_weights.append("key")
             colwise_weights.append("value")
         return colwise_weights
