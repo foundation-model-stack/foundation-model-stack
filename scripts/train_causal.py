8f7d4ed228a2aee7231bbb2d37b7c304db52cd2d
Antoni Viros <aviros@ibm.com>
2024-03-06 18:17:57 +0000

    Update (base update)
    [ghstack-poisoned]

--- scripts/train_causal.py
+++ scripts/train_causal.py
@@ -1,17 +1,20 @@
 import argparse
-from contextlib import nullcontext
 import os
+from contextlib import nullcontext
 from pathlib import Path
+
 import torch
 from torch import distributed as dist
 from torch import nn
 from torch.utils.data import DataLoader
 from torch.utils.data.distributed import DistributedSampler
-from fms import models
-from fms import datasets
+
+from fms import datasets, models
 from fms.models.hf.utils import to_hf_api
+from fms.training import plugins as trainplugins
+from fms.training import trainer
 from fms.utils import print0, tokenizers
-from fms.training import trainer, plugins as trainplugins
+
 
 #
 # This is a fairly minimal training/tuning script for causal language models.
@@ -137,13 +140,19 @@ if device_type is None:
     else:
         device_type = "cpu"
 
-device = torch.device(device_type, local_rank)
+if device_type == "cuda":
+    device = torch.device(device_type, local_rank)
+    torch.cuda.set_device(device)
+else:
+    device = torch.device(device_type)
 
 group = None
 
 if args.distributed is not None:
     dist.init_process_group(backend="nccl", rank=local_rank, world_size=world_size)
     group = dist.GroupMember.WORLD
+    # Fix until PT 2.3
+    torch._C._distributed_c10d._register_process_group("default", dist.group.WORLD)
 
 
 def get_loss_fn():
@@ -190,7 +199,7 @@ def peft_model(model):
     from fms.models.hf.llama.modeling_llama_hf import HFAdaptedLLaMAForCausalLM
 
     model = to_hf_api(model)
-    from peft import get_peft_config, get_peft_model, LoraConfig
+    from peft import LoraConfig, get_peft_config, get_peft_model
     from peft.mapping import PeftModelForCausalLM
 
     lora_config = LoraConfig(
