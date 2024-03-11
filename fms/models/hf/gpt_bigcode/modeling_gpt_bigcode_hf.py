21cb9ba5acd4bec80bd70f0542e57684caea76c5
Brian Vaughan <bvaughan@ibm.com>
2024-03-11 20:11:42 +0000

    Update (base update)
    [ghstack-poisoned]

---
+++ fms/models/hf/gpt_bigcode/modeling_gpt_bigcode_hf.py
@@ -0,0 +1,95 @@
+from typing import Optional, Tuple
+
+import torch
+import torch.nn as nn
+from transformers import PretrainedConfig
+from transformers.modeling_outputs import BaseModelOutputWithPastAndCrossAttentions
+
+from fms.models.gpt_bigcode import GPTBigCode, GPTBigCodeHeadless
+from fms.models.hf.gpt_bigcode.configuration_gpt_bigcode_hf import (
+    HFAdaptedGPTBigCodeConfig,
+)
+from fms.models.hf.lm_head_mixins import LMHeadModelLMHeadMixin
+from fms.models.hf.modeling_hf_adapter import HFDecoder, HFDecoderModelArchitecture
+
+
+class HFAdaptedGPTBigCodeDecoder(HFDecoder):
+    """Adapter for the GPTBigCodeDecoder"""
+
+    def __init__(self, model: nn.Module, config: PretrainedConfig):
+        super().__init__(model, config, attention_mask_dim=3)
+
+    def _adapt(
+        self,
+        input_ids: Optional[torch.LongTensor] = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_values: Optional[Tuple[torch.Tensor]] = None,
+        use_cache: Optional[bool] = None,
+        attn_algorithm: Optional[
+            str
+        ] = None,  # this can be passed in from top most forward
+        *args,
+        **kwargs,
+    ) -> BaseModelOutputWithPastAndCrossAttentions:
+        output, cache = self.model(
+            x=input_ids,
+            mask=attention_mask,
+            position_ids=position_ids,
+            past_key_value_states=past_key_values,
+            use_cache=use_cache,
+            attn_algorithm=attn_algorithm,
+        )
+
+        return BaseModelOutputWithPastAndCrossAttentions(
+            last_hidden_state=output, past_key_values=cache
+        )
+
+
+class HFAdaptedGPTBigCodeHeadless(HFDecoderModelArchitecture):
+    """This is the Adapter for the base granite architecture"""
+
+    # attributes required by HF
+    config_class = HFAdaptedGPTBigCodeConfig
+    base_model_prefix = "hf_adapted_gpt_bigcode"
+
+    def __init__(
+        self,
+        config: PretrainedConfig,
+        decoder: Optional[GPTBigCodeHeadless] = None,
+        embedding: Optional[nn.Module] = None,
+        *args,
+        **kwargs,
+    ):
+        # in the case we have not yet received the encoder/decoder/embedding, initialize it here
+        if decoder is None or embedding is None:
+            params = config.to_dict()
+            params["pad_id"] = params.pop("pad_token_id")
+            model = GPTBigCode(**params)
+            decoder = model.base_model if decoder is None else decoder
+            embedding = model.base_model.embedding if embedding is None else embedding
+
+        # these are now huggingface compatible
+        decoder = HFAdaptedGPTBigCodeDecoder(decoder, config)
+        super().__init__(decoder, embedding, config, *args, **kwargs)
+
+
+class HFAdaptedGPTBigCodeForCausalLM(
+    LMHeadModelLMHeadMixin, HFAdaptedGPTBigCodeHeadless
+):
+    _keys_to_ignore_on_load_missing = [r"lm_head.weight"]
+    _tied_weights_keys = ["embedding.weight", "lm_head.weight"]
+
+    def __init__(self, config: HFAdaptedGPTBigCodeConfig, *args, **kwargs):
+        super().__init__(config=config, bias=False, *args, **kwargs)
+
+    @classmethod
+    def _hf_model_from_fms(
+        cls, model: nn.Module, config: HFAdaptedGPTBigCodeConfig
+    ) -> "HFAdaptedGPTBigCodeForCausalLM":
+        return cls(
+            config=config,
+            decoder=model.base_model,
+            embedding=model.base_model.embedding,
+            lm_head=model.head,
+        )
---
+++ fms/utils/tokenizers.py
@@ -0,0 +1,171 @@
+import os
+from typing import List, Optional, Union
+
+import torch
+import torch.nn.functional as F
+
+from fms import utils
+
+
+# constants for common tokenizers
+
+char_tokenizer = "char_tokenizer"
+gpt_neox_20b = "EleutherAI/gpt-neox-20b"
+gpt_neox_125m = "EleutherAI/gpt-neox-125M"
+
+
+_has_hf = utils.has_package("transformers")
+_has_sp = utils.has_package("sentencepiece")
+
+
+class BaseTokenizer:
+    """
+    A simplistic tokenizer interface duck-type compatible with HuggingFace
+    tokenizers. An implementation of this interface could be used in fm in
+    cases where we'd like to write tests that don't depend on HF.
+    """
+
+    def __init__(self, bos_id: int, eos_id: int):
+        """
+        bos_id: the ID representing the beginning-of-sentence token
+        eos_id: the ID representing the end-of-sentence token
+        """
+        self.bos_token_id = bos_id
+        self.eos_token_id = eos_id
+
+    def tokenize(self, text: str):
+        raise NotImplementedError
+
+    def convert_ids_to_tokens(self, ids: torch.LongTensor):
+        raise NotImplementedError
+
+    def convert_tokens_to_ids(self, tokens: list[str]):
+        raise NotImplementedError
+
+    def convert_tokens_to_string(self, tokens: list[str]):
+        raise NotImplementedError
+
+    def vocab_size(self) -> int:
+        raise NotImplementedError
+
+
+class CharTokenizer(BaseTokenizer):
+    """
+    This is essentially the tokenizer used by minGPT. Every character
+    is a token, tokenized as ord(c). Vocab size is 256.
+    """
+
+    def __init__(self):
+        # 2, 3 from ascii tables are "start of text" and "end of text"
+        super().__init__(2, 3)
+
+    def tokenize(self, text: str):
+        return list(text)
+
+    def convert_ids_to_tokens(self, ids: torch.LongTensor):
+        return [chr(i) for i in ids]
+
+    def convert_tokens_to_ids(self, tokens: list[str]):
+        return [ord(t) if ord(t) < 256 else 0 for t in tokens]
+
+    def convert_tokens_to_string(self, tokens: list[str]):
+        return "".join(tokens)
+
+    def vocab_size(self):
+        return 256
+
+
+class _SentencePieceTokenizer(BaseTokenizer):
+    """
+    An adapter for a sentencepiece tokenizer.
+    """
+
+    def __init__(self, path: str):
+        from sentencepiece import SentencePieceProcessor  # type: ignore
+
+        self.sp_model = SentencePieceProcessor(model_file=path)
+        super().__init__(self.sp_model.bos_id(), self.sp_model.eos_id())
+
+    def tokenize(self, text: str):
+        return self.sp_model.encode_as_pieces(text)
+
+    def convert_ids_to_tokens(self, ids: Union[List[int], torch.LongTensor]):
+        if isinstance(ids, torch.Tensor):
+            ids = ids.tolist()
+        return self.sp_model.id_to_piece(ids)
+
+    def convert_tokens_to_ids(self, tokens: list[str]):
+        return self.sp_model.piece_to_id(tokens)
+
+    def convert_tokens_to_string(self, tokens: list[str]):
+        return self.sp_model.decode(tokens)
+
+    def vocab_size(self):
+        return self.sp_model.vocab_size()
+
+
+class _HFTokenizer(BaseTokenizer):
+    """
+    An adapter over a HuggingFace tokenizer.
+    """
+
+    def __init__(self, name: str):
+        from transformers import AutoTokenizer  # type: ignore
+
+        self.tokenizer = AutoTokenizer.from_pretrained(name)
+        super().__init__(self.tokenizer.bos_token_id, self.tokenizer.eos_token_id)
+
+    def tokenize(self, text: str):
+        return self.tokenizer.tokenize(text)
+
+    def convert_ids_to_tokens(self, ids: torch.LongTensor):
+        return self.tokenizer.convert_ids_to_tokens(ids)
+
+    def convert_tokens_to_ids(self, tokens: list[str]):
+        return self.tokenizer.convert_tokens_to_ids(tokens)
+
+    def convert_tokens_to_string(self, tokens: list[str]):
+        return self.tokenizer.convert_tokens_to_string(tokens)
+
+    def vocab_size(self):
+        return self.tokenizer.get_vocab_size()
+
+
+def get_tokenizer(name: str, style: Optional[str] = None) -> BaseTokenizer:
+    """
+    Hack to get an instance of a tokenizer by name or path.
+
+    Args:
+
+    style: 'hf', 'sentencepiece', or 'fms'. If not specified, attempt to derive
+            the type based on the name.
+    """
+    if name == "char_tokenizer" and (style is None or style == "fms"):
+        return CharTokenizer()
+
+    # SentencePiece saves models as .model files.
+    # It would be better to identify the type of the file accurately, e.g. using protobuf:
+    # https://github.com/google/sentencepiece/issues/121
+    if style == "sentencepiece" or (
+        style is None
+        and len(name) >= len(".model")
+        and name[-len(".model") :] == ".model"
+    ):
+        name = os.path.expanduser(name)
+        if not os.path.exists(name):
+            raise RuntimeError(f"Could not find SentencePiece model at '{name}'")
+        if not _has_sp:
+            raise RuntimeError(
+                f"'{name}' appears to be a sentencepiece tokenizer but sentencepiece is not installed"
+            )
+        return _SentencePieceTokenizer(name)
+    if not _has_hf:
+        raise RuntimeError(
+            f"Could not find tokenizer '{name}' and HuggingFace transformers is not installed"
+        )
+    if style is None or style == "hf":
+        return _HFTokenizer(name)
+    if style is None:
+        raise RuntimeError(f"Could not find a tokenzier {name}")
+    else:
+        raise RuntimeError(f"Could not find a {style} tokenizer with name {name}")
