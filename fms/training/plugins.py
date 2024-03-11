foundation-model-stack/foundation-model-stack

commit 3435ca2acfd2de3fbd49b88795e2c427bceae0dc

Update on "accumulate training stats with correct ranks"
fixes: https://github.com/foundation-model-stack/foundation-model-stack/issues/114

[ghstack-poisoned]

fms/training/plugins.py
@@ -119,7 +119,11 @@ class MetricReporter(TrainerPlugin):
     # TODO: add `writer` functions that handles logging metrics to experiment
     # tracking tools such as aimstack/wandb/neptune (or add alternate plugin?)
     def __init__(
-        self, seconds=10, group: dist.ProcessGroup = None, device="cpu", writer=print0
+        self,
+        seconds=10,
+        group: Optional[dist.ProcessGroup] = None,
+        device="cpu",
+        writer=print0,
     ):
         super().__init__(1)
         self.seconds = seconds

commit ca2295d997d3cf49a2964559f9e01f14253e693f

Fix a race condition
A bug was introduced by
https://github.com/foundation-model-stack/foundation-model-stack/pull/207

Since we report every `seconds` seconds, but now introduce a
dist.barrier via all_reduce, in any case where the current time doesn't
exactly match across hosts there can be a deadlock. Using average
seconds per step (and aggregating the average across hosts) fixes this.

ghstack-source-id: 2991d5d74d5c5a402f39537c1b1d1518781e56ed
Pull Request resolved: https://github.com/ibm-pytorch/foundation-model-stack/pull/213

fms/training/plugins.py
@@ -131,6 +131,7 @@ class MetricReporter(TrainerPlugin):
         self.tokens_seen = torch.tensor(0.0, device=device)
         self.last_reported_step = -1
         self.cum_loss = torch.tensor(0.0, device=device)
+        self.time_per_step = torch.tensor(1.0, device=device)
         self.last_step = -1
         self.group = group
         self.writer = writer
@@ -158,8 +159,13 @@ class MetricReporter(TrainerPlugin):
             self.last_reported_step = 0
         else:
             self.last_step = step
-            if elapsed < self.seconds or step == self.last_reported_step:
+            if step == self.last_reported_step:
                 return
+            steps_taken = step - self.last_reported_step
+            if steps_taken * self.time_per_step < self.seconds:
+                return
+            time_per_step = elapsed / (step - self.last_reported_step)
+            self.time_per_step.fill_(time_per_step)
             steps = step - self.last_reported_step
             self.last_reported_step = step
 
@@ -167,6 +173,8 @@ class MetricReporter(TrainerPlugin):
         if world > 1 and self.tokens_seen.device.type == "cuda":
             dist.all_reduce(self.tokens_seen, op=dist.ReduceOp.SUM)
             dist.all_reduce(self.cum_loss, op=dist.ReduceOp.SUM)
+            dist.all_reduce(self.time_per_step, op=dist.ReduceOp.SUM)
+            self.time_per_step /= world
 
         to_report = {}
         if "loss" in metrics:
@@ -175,6 +183,7 @@ class MetricReporter(TrainerPlugin):
 
         more_metrics = {
             "tok/stp": f"{self.tokens_seen.item() / steps:,.1f}",
+            "s/stp": f"{self.time_per_step.item():,.3f}",
             "tok/gpu/s": f"{self.tokens_seen.item() / elapsed / world:,.1f}",
         }
         if torch.cuda.is_available() and utils.has_package("pynvml"):