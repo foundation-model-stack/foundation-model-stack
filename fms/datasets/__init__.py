c52d184409e1d45ad4d72b63e7a7d4c8167c3128
Brian Vaughan <bvaughan@ibm.com>
2024-03-22 18:05:01 +0000

    Update (base update)
    [ghstack-poisoned]

---
+++ fms/datasets/__init__.py
@@ -0,0 +1,62 @@
+from typing import Any, Callable, List, Mapping, Optional
+
+import torch
+from torch.utils.data import Dataset, IterableDataset
+
+from fms.datasets import arrow, text
+from fms.datasets.instructions import JsonInstructions
+from fms.utils.tokenizers import BaseTokenizer
+
+
+def _arrow_ds_generator(data, tokenizer, **kwargs):
+    return arrow.ArrowFilesDataset(data, **kwargs)
+
+
+class MockDataset(IterableDataset):
+    def __init__(self, data, tokenizer: BaseTokenizer, max_seq_len=4096):
+        self.tokenizer = tokenizer
+        self.data = data
+        self.max_seq_len = max_seq_len
+        self.last_val = 0
+
+    def nextval(self):
+        self.last_val += 1
+        self.last_val = self.last_val % self.tokenizer.vocab_size()
+        return self.last_val
+
+    def __iter__(self):
+        while True:
+            t = torch.tensor([self.nextval() for _ in range(self.max_seq_len)])
+            yield t, t
+
+
+__dataset_factory: Mapping[str, Callable[[str, BaseTokenizer], Dataset] | type] = {
+    "instruction": JsonInstructions,
+    "text": text.causaltext,
+    "arrow": _arrow_ds_generator,
+    "mock": MockDataset,
+}
+
+
+def get_dataset(name: str, tokenizer: BaseTokenizer, data: str = "", **kwargs):
+    """
+    Get a dataset by type.
+
+    Args:
+    name: Which style of dataset to use. E.g. instruction, text.
+    data: The data this dataset should load. E.g. a url or file system path.
+    """
+    name = name.lower()
+    if name not in __dataset_factory:
+        raise NameError(f"Dataset name should be one of {__dataset_factory.keys()}")
+    ctr = __dataset_factory[name]
+    return ctr(data, tokenizer, **kwargs)
+
+
+# avoid circular dependency
+from fms.datasets.util import (
+    PackedSequenceDataset,
+    RestartableFromMapDataset,
+    SavableDataset,
+    WithSeparatorDataset,
+)
---
+++ tests/utils/test_generate.py
@@ -0,0 +1,77 @@
+import torch
+import torch.nn.functional as F
+from torch import nn
+
+from fms.utils.generation import generate, truncate_after_eos
+from fms.utils.tokenizers import get_tokenizer
+
+
+class ModelMock(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, inputs, **kwargs):
+        results = []
+        for i in range(inputs.shape[0]):
+            results.append(self.forward_one(inputs[i], **kwargs))
+        return torch.stack(results, 0)
+
+    def forward_one(self, inputs, **kwargs):
+        inputs = inputs.view(inputs.numel())
+        inputs = torch.cat((inputs[1:], torch.tensor([inputs[-1] + 1])), -1)
+        inputs[inputs > 255] = 0
+        inputs = F.one_hot(inputs, 256).float()
+        return inputs
+
+
+def test_generate():
+    _model_mock = ModelMock()
+    tokenizer = get_tokenizer("char_tokenizer")
+    ids = tokenizer.convert_tokens_to_ids(tokenizer.tokenize("ABCDE"))
+    ids = torch.tensor(ids)
+    result = generate(_model_mock, ids, max_new_tokens=5, do_sample=False)
+    result = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(result))
+    assert result == "ABCDEFGHIJ"
+
+    result = generate(
+        _model_mock, ids, max_new_tokens=5, do_sample=True, temperature=0.01
+    )
+    result = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(result))
+    assert result == "ABCDEFGHIJ"
+
+    result = generate(_model_mock, ids, max_new_tokens=5, do_sample=True, temperature=5)
+    result = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(result))
+    assert result != "ABCDEFGHIJ"
+
+
+def test_batched():
+    _model_mock = ModelMock()
+    tokenizer = get_tokenizer("char_tokenizer")
+    ids = tokenizer.convert_tokens_to_ids(tokenizer.tokenize("ABCDE"))
+    first = torch.tensor(ids)
+    second = torch.tensor(ids)
+    ids = torch.stack((first, second), dim=0)
+    result = generate(_model_mock, ids, max_new_tokens=5, do_sample=False)
+    assert torch.allclose(result[0], result[1])
+    result = tokenizer.convert_tokens_to_string(
+        tokenizer.convert_ids_to_tokens(result[0])
+    )
+    assert result == "ABCDEFGHIJ"
+
+    result = generate(
+        _model_mock, ids, max_new_tokens=5, do_sample=True, temperature=0.01
+    )
+    assert torch.allclose(result[0], result[1])
+    result = tokenizer.convert_tokens_to_string(
+        tokenizer.convert_ids_to_tokens(result[0])
+    )
+    assert result == "ABCDEFGHIJ"
+
+
+def test_truncate():
+    result = torch.ones(20)
+    result[10] = 5
+    result = truncate_after_eos(result, 5)
+    expected = torch.ones(11)
+    expected[10] = 5
+    torch.testing.assert_close(result, expected)
