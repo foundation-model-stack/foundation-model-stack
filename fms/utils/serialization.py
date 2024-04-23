1c32ace9b74aab34b38123c9530ebd90af5e24ed
Puneesh Khanna <pkhanna@habana.ai>
2024-04-23 11:26:01 +0530

    Add map location in torch load

--- fms/utils/serialization.py
+++ fms/utils/serialization.py
@@ -219,7 +219,7 @@ def load_state_dict(
     else:
         with torch.no_grad():
             checkpoint_sds = [
-                torch.load(str(ckpt_path), mmap=True) for ckpt_path in checkpoints
+                torch.load(str(ckpt_path), map_location=initial_device, mmap=True) for ckpt_path in checkpoints
             ]
     return ChainMap(*checkpoint_sds)
 
