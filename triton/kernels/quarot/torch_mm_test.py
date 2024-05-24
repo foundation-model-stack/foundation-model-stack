e9dfbe15769aa4fd890d3b00a79fab20b86a3795
RishiAstra <40644327+RishiAstra@users.noreply.github.com>
2024-05-24 17:02:15 -0400

    test R and R-1 orthogonality and more

--- triton/kernels/quarot/torch_mm_test.py
+++ triton/kernels/quarot/torch_mm_test.py
@@ -39,7 +39,7 @@ def random_rotation_almost_hadamart(size: int, use_hardcoded=False):
                 #         avg_row_dot_prod += dot_prod
                 #     if not tests_passed:
                 #         break
-                
+
                 for _ in range(num_orthogonality_tests):
                     i, j = 0, 0
                     while i == j:
@@ -51,16 +51,15 @@ def random_rotation_almost_hadamart(size: int, use_hardcoded=False):
                 avg_row_dot_prod /= (size - 1) * (size - 2)
                 if not tests_passed:
                     continue
-                
+
                 # print(f"m: {m}")
                 # since this isn't quite a hadamard matrix, it might have an extreme inverse
                 # if it's too extreme, it could cause inf in float16 when multiplied
                 m_inv = torch.inverse(m).type(torch.float16)
                 # TODO: determine what max value is acceptable
-                if torch.max(torch.square(m_inv).sum(dim=1)) < max_allowed_inv_value:
-                    potential.append((m, avg_row_dot_prod))
-                # else:
-                #     print("random matrix was bad, regenerating...")
+                # if torch.max(torch.square(m_inv).sum(dim=1)) < max_allowed_inv_value:
+                potential.append((m, avg_row_dot_prod))
+                # else:       random matrix was bad, regenerating...")
             except Exception as e:
                 print(e)
                 pass
@@ -71,7 +70,7 @@ def random_rotation_almost_hadamart(size: int, use_hardcoded=False):
     m_inv = m_inv.type(torch.float16)
     return m, m_inv
 
-def quantize_mm_avg_err(x, w, print_quantized=False):
+def quantize_mm_avg_err(x, w):
     # make sure x and w are fp16
     x = x.type(torch.float16)
     w = w.type(torch.float16)
@@ -79,16 +78,71 @@ def quantize_mm_avg_err(x, w, print_quantized=False):
     x_q, x_qs = quantize(x)
     w_q, w_qs = quantize(w)
 
-    if print_quantized:
-        print(f"x quantized: {x_q}")
-        print(f"w quantized: {w_q}")
-
     # get truth/actual result for fp16
     c = (x @ w)
     # get int8 quantized result
     # for CPU, we need to cast to int16 otherwise it won't accumilate in int16
     c_q = x_q.type(torch.int16) @ w_q.type(torch.int16)
     c_q = dequantize(c_q, x_qs * w_qs)
+
+    c_diff = torch.abs(c.type(torch.float64) - c_q.type(torch.float64))
+    c_diff_frac = torch.abs(c_diff.type(torch.float64) / (c.type(torch.float64) + 0.001)) # TODO: silly fix
+    c_diff_avg = torch.mean(c_diff.type(torch.float64), (0, 1))
+    c_diff_frac_avg = torch.mean(c_diff_frac.type(torch.float64), (0, 1))
+    c_diff_max = torch.max(c_diff.type(torch.float64))
+    c_diff_frac_max = torch.max(c_diff_frac.type(torch.float64))
+
+    return c, c_q, c_diff, c_diff_frac, c_diff_avg, c_diff_frac_avg, c_diff_max, c_diff_frac_max
+
+def quantize_mm_activ_avg_err(x, w, activ):
+    # make sure x and w are fp16
+    x = x.type(torch.float16)
+    w = w.type(torch.float16)
+    # quantize to int8
+    x_q, x_qs = quantize(x)
+    w_q, w_qs = quantize(w)
+
+    # get truth/actual result for fp16
+    c = activ(x @ w)
+    # get int8 quantized result
+    # for CPU, we need to cast to int16 otherwise it won't accumilate in int16
+    c_q = x_q.type(torch.int16) @ w_q.type(torch.int16)
+    c_q = activ(dequantize(c_q, x_qs * w_qs))
+
+    c_diff = torch.abs(c.type(torch.float64) - c_q.type(torch.float64))
+    c_diff_frac = torch.abs(c_diff.type(torch.float64) / (c.type(torch.float64) + 0.001)) # TODO: silly fix
+    c_diff_avg = torch.mean(c_diff.type(torch.float64), (0, 1))
+    c_diff_frac_avg = torch.mean(c_diff_frac.type(torch.float64), (0, 1))
+    c_diff_max = torch.max(c_diff.type(torch.float64))
+    c_diff_frac_max = torch.max(c_diff_frac.type(torch.float64))
+
+    return c, c_q, c_diff, c_diff_frac, c_diff_avg, c_diff_frac_avg, c_diff_max, c_diff_frac_max
+
+def quantize_ffn_err(x, wg, wu, wd, activ):
+    # make sure x and w are fp16
+    x = x.type(torch.float16)
+    wg = wg.type(torch.float16)
+    wu = wu.type(torch.float16)
+    wd = wd.type(torch.float16)
+
+    # quantize to int8
+    x_q, x_qs = quantize(x)
+    wg_q, wg_qs = quantize(wg)
+    wu_q, wu_qs = quantize(wu)
+    wd_q, wd_qs = quantize(wd)
+
+    xwg = x @ wg
+    xwu = x @ wu
+    xwg_activ = activ(xwg)
+    temp = xwg_activ * xwu
+    
+
+    # get truth/actual result for fp16
+    c = activ(x @ w)
+    # get int8 quantized result
+    # for CPU, we need to cast to int16 otherwise it won't accumilate in int16
+    c_q = x_q.type(torch.int16) @ w_q.type(torch.int16)
+    c_q = activ(dequantize(c_q, x_qs * w_qs))
 
     c_diff = torch.abs(c.type(torch.float64) - c_q.type(torch.float64))
     c_diff_frac = torch.abs(c_diff.type(torch.float64) / (c.type(torch.float64) + 0.001)) # TODO: silly fix
@@ -155,7 +209,7 @@ def print_test_results(results, test_name='test'):
     print("==========================")
     assert diff_avg != torch.inf
 
-m, k, n = 128, 128, 128
+m, k, n = 1024, 1024, 1024 # 4096, 4096, 4096
 
 # seed = 3
 # torch.manual_seed(seed)
@@ -164,6 +218,7 @@ runs_per_test = 1
 
 results_basic = []
 results_rot_rand = []
+results_rot_transp_rand = []
 results_rot_hard = []
 
 x_results_basic = []
@@ -191,11 +246,11 @@ for i in range(0, runs_per_test):
     x = torch.randn((m, k), dtype=torch.float16).uniform_(-0.1, 0.1)
     for i in range(m * k // 100):
         i, j = random.randrange(0, m), random.randrange(0, k)
-        x[i, j] = random.uniform(-1, 1)
+        x[i, j] = random.uniform(-0.4, 0.4)
     # w = torch.randn((k, n), dtype=torch.float16).uniform_(-0.1, 0.1)
     w = torch.tensor(np.random.normal(0, 0.1, (k, n)), dtype=torch.float16)
 
-    r_rand, r_inv_rand = random_rotation_almost_hadamart(k, use_hardcoded=False)   
+    r_rand, r_inv_rand = random_rotation_almost_hadamart(k, use_hardcoded=False)
     r_hard, r_inv_hard = random_rotation_almost_hadamart(k, use_hardcoded=True)
 
     all_x += x.reshape(-1)
@@ -205,6 +260,7 @@ for i in range(0, runs_per_test):
     w_rot_rand = r_inv_rand.type(torch.float64) @ w.type(torch.float64)
     x_rot_hard = x @ r_hard
     w_rot_hard = r_inv_hard.type(torch.float64) @ w.type(torch.float64)
+    w_rot_transp_rand = r_rand.T.type(torch.float64) @ w.type(torch.float64)
 
     all_x_rot_rand += x_rot_rand.reshape(-1)
     all_x_rot_hard += x_rot_hard.reshape(-1)
@@ -221,8 +277,16 @@ for i in range(0, runs_per_test):
 
     # TODO: generate rotation matrices every time
     # results_basic.append(quantize_mm_avg_err(x, w))
-    # results_rot_rand.append(quantize_mm_avg_err(x @ r_rand, r_inv_rand.type(torch.float64) @ w.type(torch.float64)))
-    # results_rot_hard.append(quantize_mm_avg_err(x @ r_hard, r_inv_hard.type(torch.float64) @ w.type(torch.float64)))
+    # results_rot_rand.append(quantize_mm_avg_err(x_rot_rand, w_rot_rand))
+    # results_rot_transp_rand.append(quantize_mm_avg_err(x_rot_rand, w_rot_transp_rand))
+    # results_rot_hard.append(quantize_mm_avg_err(x_rot_hard, w_rot_hard))
+
+    # softmax = lambda x: torch.nn.functional.silu(x, 1)
+    # results_basic.append(quantize_mm_activ_avg_err(x, w, softmax))
+    # results_rot_rand.append(quantize_mm_activ_avg_err(x_rot_rand, w_rot_rand, softmax))
+    # results_rot_transp_rand.append(quantize_mm_activ_avg_err(x_rot_rand, w_rot_transp_rand, softmax))
+    # results_rot_hard.append(quantize_mm_activ_avg_err(x_rot_hard, w_rot_hard, softmax))
+    
 
 # print_test_results(results_basic, 'basic quantization')
 # print_test_results(results_rot_rand, 'rotated quantization (random)')
@@ -250,5 +314,47 @@ axs[4, 0].hist(all_x_rot_rand, bins=n_bins)
 axs[4, 1].hist(all_w_rot_rand, bins=n_bins)
 axs[5, 0].hist(all_x_rot_hard, bins=n_bins)
 axs[5, 1].hist(all_w_rot_hard, bins=n_bins)
+
+# plt.show()
+
+# print_test_results(results_basic, 'basic quantization')
+# print_test_results(results_rot_rand, 'rotated quantization (random)')
+# print_test_results(results_rot_transp_rand, 'rotated quantization (random, transposed)')
+# print_test_results(results_rot_hard, 'rotated quantization (hadamard hardcoded)')
+
+
+
+
+
+
+# k = 4096
+
+# r_rand, r_inv_rand = random_rotation_almost_hadamart(k, use_hardcoded=False)
+# r_inv_rand_test = r_rand.T
+# identity = torch.eye(k)
+# test_identity = r_rand.type(torch.float64) @ r_inv_rand_test.type(torch.float64)
+# identity_diff = torch.abs(test_identity - identity)
+# print("Max identity diff:", torch.max(identity_diff))
+# print("mean identity diff:", torch.mean(identity_diff))
+
+# r_rand_dots = []
+# r_inv_rand_dots = []
+
+# comparison_count = 10000
+
+# for _ in range(comparison_count):
+#     i, j = 0, 0
+#     while i == j:
+#         i, j = random.randrange(k), random.randrange(k)
+#     dot_prod = torch.nn.functional.cosine_similarity(r_rand[:, i], r_rand[:, j], dim=0)
+#     dot_prod_inv = torch.nn.functional.cosine_similarity(r_inv_rand_test[i], r_inv_rand_test[j], dim=0)
+#     r_rand_dots.append(dot_prod)
+#     r_inv_rand_dots.append(dot_prod_inv)
+
+# fig, axs = plt.subplots(1, 2, sharey=True, tight_layout=True)
+# n_bins = 64
+
+# axs[0].hist(r_rand_dots, bins=n_bins)
+# axs[1].hist(r_inv_rand_dots, bins=n_bins)
 
 plt.show()
