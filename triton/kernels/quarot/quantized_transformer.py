masking, fixed rmsnorm, final norm

triton/kernels/quarot/quantized_transformer.py
@@ -1,7 +1,5 @@
 import json
-import math
-import random
-import numpy as np
+import sys
 import torch
 from torch import Tensor
 from torch.nn import Module
@@ -9,6 +7,7 @@ from quantized_transformer_layer import ATTNLayer, FFNLayer, QuantizedATTNLayer,
 from utils import print_test_results
 from safetensors.torch import load_file
 import sentencepiece as spm
+import utils
 
 num_heads = 32
 d_v = 128
@@ -35,6 +34,7 @@ class Transformer(Module):
                 attn_type(embedding_size, d_v * num_heads, d_k * num_heads, w_q, w_k, w_v, w_o, input_norm_weights, num_heads=num_heads),
                 ffn_type(embedding_size, ffn_intermediate_size, w_gate, w_up, w_down, ffn_norm_weights)
             ))
+        self.final_norm_weight = weights["model.norm.weight"].type(torch.float16)
 
     def forward(self, input: Tensor) -> Tensor:
         print("running forward...")
@@ -42,13 +42,12 @@ class Transformer(Module):
         for i, block in enumerate(self.blocks):
             x = block.forward(x)
             print(f"ran layer {i}", flush=True)
-        return x
+        return utils.rms_norm(x, self.final_norm_weight)
 
 
 runs_per_test = 1
 
 method_names = ["basic quantization", "hadamard", "rand rot", "rand rot transp"]
-results = [[] for _ in method_names]
 attn_layer_type: list[type[ATTNLayer]] = [QuantizedATTNLayer, QuantizedHadRotATTN, QuantizedRandRotATTN, QuantizedRandRotInvTATTN]
 ffn_layer_types: list[type[FFNLayer]] = [QuantizedFFNLayer, QuantizedHadRotFFN, QuantizedRandRotFFN, QuantizedRandRotInvTFFN]
 
@@ -62,22 +61,33 @@ for value in set(weights_map.values()):
 
 tokenizer_model_file = 'triton/kernels/quarot/test_models/granite-7b-lab/tokenizer.model'
 sp = spm.SentencePieceProcessor(model_file=tokenizer_model_file)
-hello = sp.encode('hello') # 22172
-assert len(hello) == 1
+# seq = sp.encode("Two households, both alike in")
+seq = sp.encode('Hello, this is a test of predicting the next token with a set of different schemes for making inference more efficient. This test will range across various sequence lengths.') # hello=22172
+assert len(seq) >= 32
 
 embedding_weights = weights[f"model.embed_tokens.weight"].type(torch.float16)
-test_val = embedding_weights[hello].type(torch.float16)
+all_test = embedding_weights[seq].type(torch.float16)
 
-for i in range(0, runs_per_test):
-    truth_model = Transformer(weights, ATTNLayer, FFNLayer)
-    x = test_val
-    truth = truth_model(x)
+truth_model = Transformer(weights, ATTNLayer, FFNLayer)
+test_models = [Transformer(weights, attn_type, ffn_type) for attn_type, ffn_type in zip(attn_layer_type, ffn_layer_types)]
 
-    for i, (attn_type, ffn_type) in enumerate(zip(attn_layer_type, ffn_layer_types)):
-        model = Transformer(weights, attn_type, ffn_type)
-        results[i].append((truth, model(x)))
-
-for method_name, result_list in zip(method_names, results):
-    print_test_results(result_list, method_name, embedding_weights, sp)
-
-print("done")
+test_len = 1
+while test_len <= 32:
+    test_val = all_test[:test_len]
+    results = [[] for _ in method_names]
+    
+    for i in range(0, runs_per_test):
+        x = test_val
+        truth = truth_model(x)
+        print(utils.unembed(truth, embedding_weights, sp))
+        for i, model in enumerate(test_models):
+            results[i].append((truth, model(x)))
+    
+    temp_std_out = sys.stdout
+    with open(f"results_seq_{test_len}.txt", "w") as sys.stdout:
+        for method_name, result_list in zip(method_names, results):
+            print_test_results(result_list, method_name, embedding_weights, sp)
+        print("done")
+    sys.stdout = temp_std_out
+            
+    test_len *= 2

triton/kernels/quarot/quantized_transformer_layer.py
@@ -21,7 +21,7 @@ class FFNLayer(Module):
 
     def forward(self, input: Tensor) -> Tensor:
         input = input @ self.q
-        input = input / input.square().sum(dim=1, keepdim=True).sqrt()
+        input = utils.rms_norm(input)
         input_q, input_q_s = self.quantize(input)
 
         gate_out, gate_out_s = input_q.type(self.acc_t()) @ self.w_gate.type(self.acc_t()), input_q_s * self.w_gate_s
@@ -110,7 +110,7 @@ class ATTNLayer(Module):
 
     def forward(self, input: Tensor) -> Tensor:
         input = input @ self.q
-        input = input / input.square().sum(dim=1, keepdim=True).sqrt()
+        input = utils.rms_norm(input)
         input_q, input_q_s = self.quantize(input)
 
         query_vals, query_vals_s = input_q.type(self.acc_t()) @ self.w_q.type(self.acc_t()), input_q_s * self.w_q_s
@@ -135,6 +135,8 @@ class ATTNLayer(Module):
         temp, temp_s = query_vals.type(self.acc_t()) @ key_vals.T.type(self.acc_t()), query_vals_s * key_vals_s
         temp = self.dequantize(temp, temp_s)
         temp /= math.sqrt(self.w_q.shape[-1])
+        triu = torch.triu(torch.ones(temp.shape[0], temp.shape[0], dtype=torch.half), diagonal=1)
+        temp[triu == 1] = -torch.inf
         temp = torch.softmax(temp, dim=1)
 
         # TODO: althout QuaRot doesn't seem to, we could quantize this by not dequantizing val_vals and by re-quantizing temp

triton/kernels/quarot/utils.py
@@ -107,7 +107,14 @@ def random_rotation_almost_hadamard(size: int, use_hardcoded, run_full_orthogona
         cached_rotations[size] = (m, m_inv)
         
         return m, m_inv
-    
+
+def rms_norm(x: torch.Tensor, scaling_factor=None):
+    dim1 = x.shape[1]
+    if scaling_factor is None:
+        scaling_factor = torch.ones(dim1)
+    scaling_factor = scaling_factor.view((1, -1))
+    return x * scaling_factor * (x.square().mean(dim=1, keepdim=True) + 1e-05).rsqrt()
+
 def unembed(x, embedding_weights, tokenizer: spm.SentencePieceProcessor):
     x_token_dots = x[-1].type(torch.float16) @ embedding_weights.T.type(torch.float16)
     token_id = torch.argmax(x_token_dots)