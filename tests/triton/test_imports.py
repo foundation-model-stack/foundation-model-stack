4e65dc9e8fb4dd6ede0a6e6b0c7bfef014c7d880
Antoni Viros <aviros@ibm.com>
2024-05-31 19:16:46 +0000

    remove imports before testing
    Signed-off-by: sahil suneja <sahilsuneja@gmail.com>

--- tests/triton/test_imports.py
+++ tests/triton/test_imports.py
@@ -4,6 +4,7 @@ import torch
 
 
 def test_triton_import():
+    [sys.modules.pop(k) for k in sys.modules.keys() if "fms.triton" in k]
     from fms.models import get_model
 
     fms_triton_modules = [k for k in sys.modules.keys() if "fms.triton." in k]
