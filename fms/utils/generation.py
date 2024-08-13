23e0085d8aefe97358606a2a769c9f73162a070a
Antoni Viros i Martin <aviros@ibm.com>
2024-08-13 10:58:33 -0400

    Add post iteration hook to generation
    ghstack-source-id: 00782c87339d63c8d1c1ea28cbb804f9c0bb2daf
    Pull Request resolved: https://github.com/foundation-model-stack/foundation-model-stack/pull/314
    
    Signed-off-by: Antoni Viros i Martin <aviros@ibm.com>

--- fms/utils/generation.py
+++ fms/utils/generation.py
@@ -1,3 +1,4 @@
+import logging
 import time
 from typing import Any, Callable, List, MutableMapping, Optional, Tuple, Union
 
@@ -127,6 +128,12 @@ def generate(
     contiguous_cache: bool = False,
     eos_token_id: Optional[int] = None,
     timing: str = "",
+    post_iteration_hook: Optional[
+        Callable[
+            [torch.Tensor, torch.Tensor, MutableMapping[str, Any]],
+            Tuple[torch.Tensor, MutableMapping[str, Any]],
+        ]
+    ] = None,
     extra_kwargs: Optional[MutableMapping[str, Any]] = None,
 ):
     """
@@ -154,6 +161,10 @@ def generate(
             with the following information:
             - "per-token": Array with `max_new_tokens` time measurements (in s)
             - "e2e": Array with a single e2e generation loop time measurement (in s)
+        post_iteration_hook: a function that will get called after each iteration.
+            It must have the following signature: f(Tensor logits, Tensor next_val, Dict kwargs) ->
+            Tuple[Tensor next_val, Dict kwargs]. If it is defined, will replace next_val
+            and kwargs based on the contents of the function.
         extra_kwargs: an optional mapping of additional kwargs to pass to the model.
             For example: if extra_kwargs contains position_ids and mask keys, these
             model parameters will be updated as-appropriate for each token generated.
@@ -218,6 +229,10 @@ def generate(
             next_val = torch.multinomial(probs, num_samples=1)
         else:
             next_val = torch.argmax(logits, dim=-1).unsqueeze(0).t()
+
+        # Golden reference replacement
+        if post_iteration_hook is not None:
+            next_val, kwargs = post_iteration_hook(logits, next_val, kwargs)
 
         result = torch.cat((result, next_val), dim=-1)
 
