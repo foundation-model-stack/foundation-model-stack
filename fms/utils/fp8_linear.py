e8f851ac92d5c535810a29a9ee03add847723586
Chih-Chieh-Yang <7364402+cyang49@users.noreply.github.com>
2024-08-29 14:33:04 -0400

    update kernel import to fms_extra

--- fms/utils/fp8_linear.py
+++ fms/utils/fp8_linear.py
@@ -25,85 +25,8 @@ except:
 
 HAS_VLLM_KERNELS = False
 try:
-    sys.path.append('/net/storage149/mnt/md0/ccyang/github.com/temp/vllm/build/lib.linux-x86_64-cpython-311')
-    import vllm._core_C
-    import vllm._C
+    import fms_extras._custom_ops as ops
     HAS_VLLM_KERNELS = True
-    
-    # copied from vllm
-    def scaled_fp8_quant(
-        input: torch.Tensor,
-        scale: Optional[torch.Tensor] = None,
-        num_token_padding: Optional[int] = None,
-        scale_ub: Optional[torch.Tensor] = None,
-        use_per_token_if_dynamic: bool = False,
-    ) -> Tuple[torch.Tensor, torch.Tensor]:
-        """
-        Quantize input tensor to FP8 and return quantized tensor and scale.
-
-        This function supports both static and dynamic quantization: If you
-        provide the scale, it will use static scaling and if you omit it,
-        the scale will be determined dynamically. The function also allows
-        optional padding of the output tensors for downstream kernels that
-        will benefit from padding.
-
-        Args:
-            input: The input tensor to be quantized to FP8
-            scale: Optional scaling factor for the FP8 quantization
-            scale_ub: Optional upper bound for scaling factor in dynamic 
-                per token case
-            num_token_padding: If specified, pad the first dimension
-                of the output to at least this value.
-            use_per_token_if_dynamic: Whether to do per_tensor or per_token 
-                in the dynamic quantization case.
-
-        Returns:
-            Tuple[torch.Tensor, torch.Tensor]: The output tensor in FP8 and
-                scaling factor.
-        """
-        # This code assumes batch_dim and num_tokens are flattened
-        assert (input.ndim == 2)
-        shape: Union[Tuple[int, int], torch.Size] = input.shape
-        # For rocm, the output fp8 dtype is torch.float_e3m3fnuz
-        out_dtype: torch.dtype = torch.float8_e4m3fn
-        if num_token_padding:
-            shape = (max(num_token_padding, input.shape[0]), shape[1])
-        output = torch.empty(shape, device=input.device, dtype=out_dtype)
-
-        if scale is None:
-            if use_per_token_if_dynamic:
-                scale = torch.empty((shape[0], 1),
-                                    device=input.device,
-                                    dtype=torch.float32)
-                torch.ops._C.dynamic_per_token_scaled_fp8_quant(
-                    output, input, scale, scale_ub)
-            else:
-                scale = torch.zeros(1, device=input.device, dtype=torch.float32)
-                torch.ops._C.dynamic_scaled_fp8_quant(output, input, scale)
-        else:
-            # num_token_padding not implemented for this case
-            assert (scale.numel() == 1 or num_token_padding is None)
-            torch.ops._C.static_scaled_fp8_quant(output, input, scale)
-
-        return output, scale
-    
-    def cutlass_scaled_mm(a: torch.Tensor,
-                        b: torch.Tensor,
-                        scale_a: torch.Tensor,
-                        scale_b: torch.Tensor,
-                        out_dtype: torch.dtype,
-                        bias: Optional[torch.Tensor] = None) -> torch.Tensor:
-        assert (b.shape[0] % 16 == 0 and b.shape[1] % 16 == 0)
-        assert (out_dtype is torch.bfloat16 or out_dtype is torch.float16)
-        assert bias is None or bias.shape[0] == b.shape[
-            1] and bias.dtype == out_dtype
-
-        m = a.shape[0]
-        n = b.shape[1]
-        out = torch.empty((m, n), dtype=out_dtype, device=a.device)
-        torch.ops._C.cutlass_scaled_mm(out, a, b, scale_a, scale_b, bias)
-        return out
-
 except Exception as e:
     print("!!WARNING!! VLLM kernels import failure! Use torch._scaled_mm instead.")
     pass
@@ -197,14 +120,14 @@ class AutoFp8InferenceLinear(Fp8InferenceLinearBase):
             input = input.reshape(-1, input.shape[2])
 
         if HAS_VLLM_KERNELS:
-            aq, ascale = scaled_fp8_quant(
+            aq, ascale = ops.scaled_fp8_quant(
                 input,
                 self.input_scale, # dynamic if input_scale is None; otherwise static
                 scale_ub=None,
                 use_per_token_if_dynamic=False)
 
             # Fused GEMM_DQ
-            output = cutlass_scaled_mm(aq,
+            output = ops.cutlass_scaled_mm(aq,
                                        wq.T,
                                        out_dtype=input.dtype,
                                        scale_a=ascale,
