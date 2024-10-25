659f41d11fd5ce9498afb6ab49d137aa53033850
sahil suneja <sahilsuneja@gmail.com>
2024-10-25 20:42:00 +0000

    updates
    Signed-off-by: sahil suneja <sahilsuneja@gmail.com>

--- fms/models/granite.py
+++ fms/models/granite.py
@@ -2,7 +2,7 @@ import logging
 import math
 import re
 from dataclasses import dataclass
-from typing import Any, Mapping, Optional
+from typing import Any, Mapping, Optional, Tuple
 
 import torch
 import torch.nn as nn
@@ -124,10 +124,6 @@ class GraniteBlock(nn.Module):
     ):
         # if the cache is not empty, we need to get the kv cache for self and cross attention
         self_attn_past_key_value = past_key_value_state
-        # if past_key_value_state is not None:
-        #     self_attn_past_key_value = past_key_value_state[:2]
-        # else:
-        #     self_attn_past_key_value = None
 
         # first we do MHA and Add&Norm
         residual = x
@@ -384,13 +380,13 @@ class Granite(nn.Module):
 
     def forward(
         self,
-        x,
-        mask=None,
-        position_ids=None,
-        past_key_value_states=None,
-        use_cache=False,
-        only_last_token=False,
-        attn_algorithm=None,
+        x: torch.LongTensor,
+        mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_value_states: Optional[Tuple[torch.FloatTensor,]] = None,
+        use_cache: bool = False,
+        only_last_token: bool = False,
+        attn_algorithm: Optional[str] = None,
     ):
         output, cache = self.base_model(
             x, mask, position_ids, past_key_value_states, use_cache, attn_algorithm
@@ -505,89 +501,3 @@ def _hf_sd_to_fms_sd(hf_sd: Mapping) -> Mapping:
 
 
 serialization.register_adapter("granite", "hf", _hf_sd_to_fms_sd)
-
-
-def convert_hf_granite(hf_model: "GraniteForCausalLM") -> Granite:  # type: ignore
-    """
-    Convert a Granite huggingface model to an fms model
-
-    Parameters
-    ----------
-    hf_model: GraniteForCausalLM
-        a Granite Huggingface model
-
-    Returns
-    -------
-    Granite
-        an FMS Granite model
-    """
-
-    if not _has_hf:
-        raise ImportError(
-            "in order to convert huggingface weights, you need to have transformers installed"
-        )
-
-    import re
-
-    config = GraniteConfig(
-        src_vocab_size=hf_model.config.vocab_size,
-        emb_dim=hf_model.config.hidden_size,
-        norm_eps=hf_model.config.rms_norm_eps,
-        nheads=hf_model.config.num_attention_heads,
-        nlayers=hf_model.config.num_hidden_layers,
-        hidden_grow_factor=hf_model.config.intermediate_size
-        / hf_model.config.hidden_size,
-        multiple_of=1,  # this is set to 1 as it is encoded in the hidden dimension
-        activation_fn=hf_model.config.hidden_act,
-        max_expected_seq_len=hf_model.config.max_position_embeddings,
-        rope_theta=hf_model.config.rope_theta,
-    )
-    model = Granite(config)
-    count_parameters = lambda m: sum(p.numel() for p in m.parameters())
-    assert count_parameters(model) == count_parameters(hf_model)
-
-    hf_sd = hf_model.model.state_dict()
-
-    replacements = [
-        (r"^embed_tokens.weight", "base_model.embedding.weight"),
-        (r"^norm", "base_model.dec_norm"),
-        (r"^layers", "base_model.layers"),
-        (r"self_attn\.k_proj", "attn.key"),
-        (r"self_attn\.v_proj", "attn.value"),
-        (r"self_attn\.q_proj", "attn.query"),
-        (r"self_attn\.o_proj", "attn.dense"),
-        (r"mlp\.gate_proj", "ff_sub_layer.wg"),
-        (r"mlp\.up_proj", "ff_sub_layer.w1"),
-        (r"mlp\.down_proj", "ff_sub_layer.w2"),
-        (r"input_layernorm", "ln"),
-        (r"post_attention_layernorm", "ff_ln"),
-    ]
-    new_sd = {}
-    for name, param in hf_sd.items():
-        new_name = name
-        for pattern, repl in replacements:
-            new_name = re.sub(pattern, repl, new_name)
-        new_sd[new_name] = param
-
-    model.load_state_dict(new_sd, strict=False)
-    model.shared.head.weight = hf_model.lm_head.weight
-
-    # model.rot_emb.freqs = hf_model.model.layers[0].self_attn.rotary_emb.inv_freq
-    for layer in model.layers:
-        q = layer.attn.query.weight.data
-        q = (
-            q.view(model.config.nheads, 2, -1, q.size(1))
-            .transpose(1, 2)
-            .reshape(*q.size())
-        )
-        layer.attn.query.weight.data = q
-
-        k = layer.attn.key.weight.data
-        k = (
-            k.view(model.config.nheads, 2, -1, k.size(1))
-            .transpose(1, 2)
-            .reshape(*k.size())
-        )
-        layer.attn.key.weight.data = k
-
-    return model
