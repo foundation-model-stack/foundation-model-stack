fcf9fe4f43e4f013a59e94046d0a8330e7d6cf7a
Antoni Viros i Martin <aviros@ibm.com>
2025-04-14 19:25:33 +0000

    Make more stringent conditions
    Signed-off-by: Antoni Viros i Martin <aviros@ibm.com>

--- fms/utils/generation.py
+++ fms/utils/generation.py
@@ -119,9 +119,16 @@ def _make_cache_contiguous(
     n_kv_s: List[List[torch.Tensor]] = []
     for layer_idx in range(len(past_key_value_states)):
         n_kv_s.append([])
-        if not isinstance(past_key_value_states[layer_idx], SSMCacheUnit) and (
-            not past_key_value_states[layer_idx][0].is_contiguous()
-            or not past_key_value_states[layer_idx][1].is_contiguous()
+        if (
+            isinstance(past_key_value_states[layer_idx], Iterable)
+            and (
+                isinstance(past_key_value_states[layer_idx][0], torch.Tensor)
+                and isinstance(past_key_value_states[layer_idx][1], torch.Tensor)
+            )
+            and (
+                not past_key_value_states[layer_idx][0].is_contiguous()
+                or not past_key_value_states[layer_idx][1].is_contiguous()
+            )
         ):
             for tensor_idx in range(len(past_key_value_states[layer_idx])):
                 n_kv_s[layer_idx].append(
