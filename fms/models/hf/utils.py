5f114af514a56bf9654006d8a08a1a30743e34d0
Sahil Suneja <suneja@us.ibm.com>
2025-05-08 22:06:09 +0000

    adding siglip vision support
    Signed-off-by: Sahil Suneja <suneja@us.ibm.com>

--- fms/models/hf/utils.py
+++ fms/models/hf/utils.py
@@ -244,13 +244,27 @@ def _infer_model_configuration(
         config_params["mamba_n_heads"] = config.mamba_n_heads
         config_params["use_bias"] = config.mamba_proj_bias
         config_params["norm_eps"] = config.rms_norm_eps
+    elif architecture == "SiglipModel":
+        config = config.vision_config
+        inner_dim = config.intermediate_size
+        architecture = "siglip_vision"
+        config_params["hidden_size"] = config.hidden_size
+        config_params["intermediate_size"] = config.intermediate_size
+        config_params["num_hidden_layers"] = config.num_hidden_layers
+        config_params["num_attention_heads"] = config.num_attention_heads
+        config_params["num_channels"] = config.num_channels
+        config_params["image_size"] = config.image_size
+        config_params["patch_size"] = config.patch_size
+        config_params["hidden_act"] = config.hidden_act
+        config_params["layer_norm_eps"] = config.layer_norm_eps
+        config_params["attention_dropout"] = config.attention_dropout
     else:
         raise ValueError(
             "FMS model implementations currently only support LlamaForCausalLM, GPTBigCodeForCausalLM, MixtralForCausalLM, RobertaForMaskedLM and GraniteForCausalLM"
         )
 
     # infer common params
-    config_params["src_vocab_size"] = config.vocab_size
+    config_params["src_vocab_size"] = config.vocab_size if hasattr(config, 'vocab_size') else None
     config_params["nheads"] = config.num_attention_heads
     config_params["nlayers"] = config.num_hidden_layers
     config_params["hidden_grow_factor"] = inner_dim / config.hidden_size
