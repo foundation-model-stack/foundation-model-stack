7d964ea0c04a655b8c7269c43eb8d1a3d03b04e6
Mauricio J Serrano <mserrano@us.ibm.com>
2025-07-10 10:34:52 -0400

    switch to RMSNorm from pytorch

---
+++ fms/modules/rmsnorm.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+
+
+class RMSNormFMS(torch.nn.RMSNorm):
+    """
+    A RmsNorm implementation. 
+    ...
+    Args
+    ----
+    normalized_shape : int
+        Dimensionality of input data (size of final tensor axis)
+    eps : float
+        Safety term to prevent division by zero. Make sure the chosen value fits in the range of your encoding scheme (i.e. fp16 requires eps >= 6e-8).
+        Recenter inputs around zero before normalizing, or just rescale?
+    """
+
+    def __init__(
+        self,
+        normalized_shape,
+        eps=1e-06,
+        use_high_precision_pow=False,
+    ):
+        super(RMSNormFMS, self).__init__(normalized_shape, eps)
+        self.use_high_precision_pow = use_high_precision_pow
+
+    def forward(self, x):
+        xf = x
+        if self.use_high_precision_pow:
+            xf = x.float()
+        xf = super().forward(xf)
+        x = xf.type_as(x)
+        return x
