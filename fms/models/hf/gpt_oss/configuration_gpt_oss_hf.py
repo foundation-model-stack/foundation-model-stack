733a4a9d86cd49f07d67c8f8d60e7a4f40fe3ac2
Flavia Beo <flavia.beo@ibm.com>
2025-08-19 10:42:56 -0300

    HF adapters
    Signed-off-by: Flavia Beo <flavia.beo@ibm.com>

--- fms/models/hf/gpt_oss/configuration_gpt_oss_hf.py
+++ fms/models/hf/gpt_oss/configuration_gpt_oss_hf.py
@@ -23,49 +23,49 @@ class HFAdaptedGptOssConfig(PretrainedConfig):
 
     def __init__(
         self,
-        num_hidden_layers: int = 36,
-        num_local_experts: int = 128,
-        vocab_size: int = 201088,
-        hidden_size: int = 2880,
-        intermediate_size: int = 2880,
+        num_experts: int = 128,
+        src_vocab_size: int = 201088,
+        emb_dim: int = 2880,
+        hidden_dim: int = 2880,
         head_dim: int = 64,
         num_attention_heads: int = 64,
-        num_key_value_heads: int = 8,
         sliding_window: int = 128,
-        rope_theta: float = 150000.0,
-        tie_word_embeddings=False,
-        hidden_act: str = "silu",
+        rope_base: float = 150000.0,
+        tie_heads=False,
+        activation_fn: str = "silu",
         initializer_range: float = 0.02,
-        max_position_embeddings=131072,
-        rms_norm_eps: float = 1e-5,
-        rope_scaling={"rope_type": "yarn", "factor": 32.0, "beta_fast": 32.0, "beta_slow": 1.0, "truncate": False},
-        attention_dropout: float = 0.0,
-        num_experts_per_tok=4,
+        max_expected_seq_len=131072,
+        top_k_experts=4,
         router_aux_loss_coef: float = 0.9,
         output_router_logits=False,
         use_cache=True,
         layer_types=None,
+        pad_id = 199999,
+        nheads: int = 64,
+        nlayers: int = 24,
+        dim: int = 2880,
+        norm_eps: float = 1e-05,
+        kvheads: int = 8,
+        p_dropout: float = 0.0,
         **kwargs,
     ):
-        self.vocab_size = vocab_size
-        self.hidden_size = hidden_size
-        self.intermediate_size = intermediate_size
-        self.num_hidden_layers = num_hidden_layers
-        self.num_attention_heads = num_attention_heads
-        self.num_local_experts = num_local_experts
+        self.src_vocab_size = src_vocab_size
+        self.emb_dim = emb_dim
+        self.nlayers = nlayers
+        self.nheads = nheads
+        self.num_experts = num_experts
         self.sliding_window = sliding_window
-        self.num_experts_per_tok = num_experts_per_tok
+        self.top_k_experts = top_k_experts
         # for backward compatibility
         if num_key_value_heads is None:
             num_key_value_heads = num_attention_heads
 
         self.num_key_value_heads = num_key_value_heads
-        self.hidden_act = hidden_act
+        self.activation_fn = activation_fn
         self.initializer_range = initializer_range
-        self.rms_norm_eps = rms_norm_eps
-        self.rope_theta = rope_theta
-        self.rope_scaling = rope_scaling
-        self.attention_dropout = attention_dropout
+        self.norm_eps = norm_eps
+        self.rope_base = rope_base
+        self.p_dropout = p_dropout
         self.head_dim = head_dim if head_dim is not None else self.hidden_size // self.num_attention_heads
         self.layer_types = layer_types
         if self.layer_types is None:
@@ -79,12 +79,12 @@ class HFAdaptedGptOssConfig(PretrainedConfig):
             self.rope_scaling["rope_type"] = self.rope_scaling["type"]
 
         self.attention_bias = True
-        self.max_position_embeddings = max_position_embeddings
+        self.max_expected_seq_len = max_expected_seq_len
         self.router_aux_loss_coef = router_aux_loss_coef
         self.output_router_logits = output_router_logits
         self.use_cache = use_cache
         super().__init__(
-            tie_word_embeddings=tie_word_embeddings,
+            tie_word_embeddings=tie_heads,
             **kwargs,
         )
 
