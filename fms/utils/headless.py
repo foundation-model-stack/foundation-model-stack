3362c5fbcfd5e6314320adafcbff9c243b97c145
Joshua Rosenkranz <jmrosenk@us.ibm.com>
2025-09-25 14:10:22 +0000

    behavior is now backwards compatible
    Signed-off-by: Joshua Rosenkranz <jmrosenk@us.ibm.com>

--- fms/utils/headless.py
+++ fms/utils/headless.py
@@ -9,19 +9,19 @@ def gather_outputs(
     General logic to gather the outputs of the base headless model for decoder models
     """
 
-    # added for deprecation
+    # added for deprecation (returns a 2d tensor for backwards compatibilty)
     if last_n_tokens == 0 and only_last_token:
-        last_n_tokens = 1
         # add tracing check here as the warnings will cause re-compilations
         is_compiling = torch.compiler.is_compiling()
         if not is_compiling:
             warnings.warn(
-                "only_last_token will be deprecated in future versions, use last_n_tokens instead. Returned shape will now be 3d.",
+                "only_last_token will be deprecated in future versions, use last_n_tokens instead.",
                 DeprecationWarning,
                 stacklevel=2,
             )
-
-    if last_n_tokens > 0 and base_model_output.shape[1] >= last_n_tokens:
+        base_model_output = base_model_output[:, -1, :]
+    # this is the base case
+    elif last_n_tokens > 0 and base_model_output.shape[1] >= last_n_tokens:
         base_model_output = base_model_output[:, -last_n_tokens:, :]
 
     return base_model_output
