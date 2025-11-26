4b1e31866c565d76e5cbc174e5c2c9cf35aba397
Alex-Brooks <Alex.Brooks@ibm.com>
2025-11-26 10:35:36 +0000

    no wild imports in builder imports
    Signed-off-by: Alex-Brooks <Alex.Brooks@ibm.com>

--- fms/models/hf/config_utils/__init__.py
+++ fms/models/hf/config_utils/__init__.py
@@ -1,33 +1,33 @@
 from functools import partial
 from fms.models.hf.config_utils.config_registry import ModelConfigRegistry
-from fms.models.hf.config_utils.param_builders import *
+from fms.models.hf.config_utils import param_builders as pb
 
 # Any models that should be initializable from HF params should be added here.
 _FMS_MODEL_CONFIG_REGISTRY = ModelConfigRegistry(
     {
-        "LlamaForCausalLM": ("llama", build_llama_params),
-        "GPTBigCodeForCausalLM": ("gpt_bigcode", build_gpt_bigcode_params),
-        "MixtralForCausalLM": ("mixtral", build_mixtral_params),
-        "RobertaForMaskedLM": ("roberta", build_roberta_params),
+        "LlamaForCausalLM": ("llama", pb.build_llama_params),
+        "GPTBigCodeForCausalLM": ("gpt_bigcode", pb.build_gpt_bigcode_params),
+        "MixtralForCausalLM": ("mixtral", pb.build_mixtral_params),
+        "RobertaForMaskedLM": ("roberta", pb.build_roberta_params),
         "RobertaForQuestionAnswering": (
             "roberta_question_answering",
-            build_roberta_params,
+            pb.build_roberta_params,
         ),
-        "GraniteForCausalLM": ("granite", build_granite_params),
-        "MistralForCausalLM": ("mistral", build_mistral_params),
-        "BambaForCausalLM": ("bamba", build_bamba_params),
-        "SiglipModel": ("siglip_vision", build_siglip_vision_params),
-        "LlavaNextForConditionalGeneration": ("llava_next", build_llava_next_params),
-        "MPNetForMaskedLM": ("mpnet", build_mpnet_params),
-        "BertForMaskedLM": ("bert", build_bert_params),
+        "GraniteForCausalLM": ("granite", pb.build_granite_params),
+        "MistralForCausalLM": ("mistral", pb.build_mistral_params),
+        "BambaForCausalLM": ("bamba", pb.build_bamba_params),
+        "SiglipModel": ("siglip_vision", pb.build_siglip_vision_params),
+        "LlavaNextForConditionalGeneration": ("llava_next", pb.build_llava_next_params),
+        "MPNetForMaskedLM": ("mpnet", pb.build_mpnet_params),
+        "BertForMaskedLM": ("bert", pb.build_bert_params),
         # Classify arches have some extra keys for labels
         "RobertaForSequenceClassification": (
             "roberta_classification",
-            partial(build_roberta_params, is_classify=True),
+            partial(pb.build_roberta_params, is_classify=True),
         ),
         "BertForSequenceClassification": (
             "bert_classification",
-            partial(build_bert_params, is_classify=True),
+            partial(pb.build_bert_params, is_classify=True),
         ),
     }
 )
