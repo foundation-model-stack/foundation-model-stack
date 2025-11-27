de201612b2cb0ef9ca817a9adddfc348c7ec3805
Alex-Brooks <Alex.Brooks@ibm.com>
2025-11-27 09:47:07 +0000

    Add docstrings and type hints
    Signed-off-by: Alex-Brooks <Alex.Brooks@ibm.com>

--- fms/models/hf/config_utils/__init__.py
+++ fms/models/hf/config_utils/__init__.py
@@ -27,26 +27,27 @@ directly here to make the boundary with subconfigs less awkward.
 from functools import partial
 from fms.models.hf.config_utils.config_registry import ModelConfigRegistry
 from fms.models.hf.config_utils import param_builders as pb
+from fms.models.hf.config_utils.config_utils_types import RegistryMap
 
 # Any models that should be initializable from HF params should be added here.
 # fmt: off
-_FMS_MODEL_CONFIG_REGISTRY = ModelConfigRegistry(
-    {
-        "LlamaForCausalLM": ("llama", pb.build_llama_params),
-        "GPTBigCodeForCausalLM": ("gpt_bigcode", pb.build_gpt_bigcode_params),
-        "MixtralForCausalLM": ("mixtral", pb.build_mixtral_params),
-        "RobertaForMaskedLM": ("roberta", pb.build_roberta_params),
-        "RobertaForQuestionAnswering": ("roberta_question_answering", pb.build_roberta_params),
-        "GraniteForCausalLM": ("granite", pb.build_granite_params),
-        "MistralForCausalLM": ("mistral", pb.build_mistral_params),
-        "BambaForCausalLM": ("bamba", pb.build_bamba_params),
-        "SiglipModel": ("siglip_vision", pb.build_siglip_vision_params),
-        "LlavaNextForConditionalGeneration": ("llava_next", pb.build_llava_next_params),
-        "MPNetForMaskedLM": ("mpnet", pb.build_mpnet_params),
-        "BertForMaskedLM": ("bert", pb.build_bert_params),
-        # Classify arches have some extra keys for labels
-        "RobertaForSequenceClassification": ("roberta_classification", partial(pb.build_roberta_params, is_classify=True)),
-        "BertForSequenceClassification": ("bert_classification", partial(pb.build_bert_params, is_classify=True)),
-    }
-)
+_FMS_MODEL_REGISTRY_MAP: RegistryMap = {
+    "LlamaForCausalLM": ("llama", pb.build_llama_params),
+    "GPTBigCodeForCausalLM": ("gpt_bigcode", pb.build_gpt_bigcode_params),
+    "MixtralForCausalLM": ("mixtral", pb.build_mixtral_params),
+    "RobertaForMaskedLM": ("roberta", pb.build_roberta_params),
+    "RobertaForQuestionAnswering": ("roberta_question_answering", pb.build_roberta_params),
+    "GraniteForCausalLM": ("granite", pb.build_granite_params),
+    "MistralForCausalLM": ("mistral", pb.build_mistral_params),
+    "BambaForCausalLM": ("bamba", pb.build_bamba_params),
+    "SiglipModel": ("siglip_vision", pb.build_siglip_vision_params),
+    "LlavaNextForConditionalGeneration": ("llava_next", pb.build_llava_next_params),
+    "MPNetForMaskedLM": ("mpnet", pb.build_mpnet_params),
+    "BertForMaskedLM": ("bert", pb.build_bert_params),
+    # Classify arches have some extra keys for labels
+    "RobertaForSequenceClassification": ("roberta_classification", partial(pb.build_roberta_params, is_classify=True)),
+    "BertForSequenceClassification": ("bert_classification", partial(pb.build_bert_params, is_classify=True)),
+}
 # fmt: on
+
+_FMS_MODEL_CONFIG_REGISTRY = ModelConfigRegistry(_FMS_MODEL_REGISTRY_MAP)
--- fms/models/hf/config_utils/config_registry.py
+++ fms/models/hf/config_utils/config_registry.py
@@ -4,6 +4,8 @@ Model specific utils for converting HF PretrainedConfig objects -> FMS kwargs.
 
 import logging
 from fms.models import list_variants
+from fms.models.hf.config_utils.config_utils_types import ParamBuilderFunc, RegistryMap
+from transformers import PretrainedConfig
 
 logger = logging.getLogger(__name__)
 
@@ -11,19 +13,41 @@ logger = logging.getLogger(__name__)
 class ModelConfigRegistry:
     """Wrapper class that handles converting hf config -> FMS kwargs."""
 
-    def __init__(self, registry_map):
-        self.model_param_builders = {}
-        self.model_arch_mappings = {}
+    def __init__(self, registry_map: RegistryMap):
+        self.model_param_builders: dict[str, ParamBuilderFunc] = {}
+        self.model_arch_mappings: dict[str, str] = {}
 
         # Default registry initialization for all in tree models
         for hf_arch_name, fms_info in registry_map.items():
             self.register_model_arch_info(hf_arch_name, *fms_info)
 
-    def register_model_arch_info(self, hf_arch_name, fms_arch_name, param_builder):
+    def register_model_arch_info(
+        self, hf_arch_name: str, fms_arch_name: str, param_builder: ParamBuilderFunc
+    ):
+        """
+        Register the transformers architecture dicts mapping to
+        the builder for retrieving the FMS model's config param dict,
+        as well as the fms arch name. Note that currently we always use
+        the first listed architecture as the key, but in the future, we
+        could use a tuple of HF architectures as keys.
+
+        Args:
+        hf_arch_name: arch name in HF Transformers.
+        fms_arch_name: arch name in FMS.
+        param_builder: Func for this model which takes exactly one arg
+            (pretrained config) and returns the model's config params dict.
+        """
         self.model_param_builders[hf_arch_name] = param_builder
         self.model_arch_mappings[hf_arch_name] = fms_arch_name
 
-    def map_hf_to_fms_arch(self, architecture):
+    def map_hf_to_fms_arch(self, architecture: str) -> str:
+        """
+        Map the transformers architecture to the FMS architecture.
+
+        Args:
+        architecture: transformers architecture; note that if multiple
+            are present, we typically pass architectures[0] currently.
+        """
         if architecture in self.model_arch_mappings:
             fms_arch = self.model_arch_mappings[architecture]
             return fms_arch
@@ -31,7 +55,18 @@ class ModelConfigRegistry:
             f"HF architecture {architecture} is unsupported! Registered architectures: {list(self.model_arch_mappings.keys())}"
         )
 
-    def map_hf_arch_to_fms_params(self, architecture, config):
+    def map_hf_arch_to_fms_params(
+        self, architecture: str, config: PretrainedConfig
+    ) -> dict:
+        """
+        Map the transformers architecture to the callable that produces
+        the config params dict to be passed as additional kwargs when
+        initializing the FMS model.
+
+        Args:
+        architecture: transformers architecture; note that if multiple
+            are present, we typically pass architectures[0] currently.
+        """
         # Map HF model config to FMS model config
         if architecture in self.model_arch_mappings:
             param_builder = self.model_param_builders[architecture]
@@ -41,15 +76,20 @@ class ModelConfigRegistry:
             f"HF architecture {architecture} is unsupported! Registered architectures: {list(self.model_arch_mappings.keys())}"
         )
 
-    def hf_config_to_fms_config_params(self, config, model_path):
-        architecture = config.architectures[0]
+    def hf_config_to_fms_config_params(
+        self, config: PretrainedConfig, model_path: str
+    ) -> dict:
+        if config.architectures is None:
+            raise ValueError("HF Config must contain a model architecture")
+
         if len(config.architectures) > 1:
             logger.warning(
                 "The provided HF config supports multiple architectures; only the first, "
                 "%s, will be used in building the FMS model's config params"
-                % architecture
+                % config.architectures[0]
             )
 
+        architecture = config.architectures[0]
         config_params = self.map_hf_arch_to_fms_params(architecture, config)
         fms_arch = self.map_hf_to_fms_arch(architecture)
 
